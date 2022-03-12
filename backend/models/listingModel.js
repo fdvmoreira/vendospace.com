b983b145ddb1ad07b990f7f2f6abecddf5d10de9
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-03-12 21:41:33 +0000

    create listing model

---
+++ backend/models/listingModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const listingSchema = new mongoose.Schema({
+    space: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Space'
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: {
+            values: [''], // todo - listing status here 
+            message: '{VALUE} not supported here'
+        }
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Listing', listingSchema);
