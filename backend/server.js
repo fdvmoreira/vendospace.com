ab7e6f5b2333b336135edf951777ec737d62ce6d
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-03-12 23:51:38 +0000

    implement custom error handler middleware for server errors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 require("colors");
 const { listen } = require("express/lib/application");
+const req = require("express/lib/request");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+
+// database connection
+//require('./config/db')();
+
+// use custom error handler 'errorHandler' middleware
+app.use(require('./middlewares/errorHandler'));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`.bold.cyan.underline));
