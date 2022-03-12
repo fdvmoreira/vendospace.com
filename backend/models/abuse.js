4171db2952115dd2a2b8ce8f9f30934fe82160e9
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-03-12 18:28:20 +0000

    create Abuse Schema to store reported abuses

---
+++ backend/models/abuse.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const abuseSchema = new mongoose.Schema({
+    abuseId: String,
+    itemReportedId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Item' // todo - modify to link to the correct Object
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    userReported: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    abuseTye: {
+        type: String,
+        enum: {
+            values: ['Other'], // todo types of abuses here
+            message: '{VALUE} is not supported'
+        },
+        required: true
+    },
+    message: String
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Abuse', abuseSchema);
