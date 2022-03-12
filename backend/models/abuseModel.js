596aff392e316b5b8487e08235f96ef770d2ad68
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-03-12 19:14:36 +0000

    remove custom id because the mongoose module will create a default id for each document

--- backend/models/abuseModel.js
+++ backend/models/abuseModel.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const abuseSchema = new mongoose.Schema({
-    abuseId: String,
     itemReportedId: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
