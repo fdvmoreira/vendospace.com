0df87b4e12dbe57d073c6e74c4bb8366e35f88a8
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-03-12 19:27:30 +0000

    create message model to represent each text exchange beteween users

---
+++ backend/models/messageModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    text: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Message', messageSchema);
