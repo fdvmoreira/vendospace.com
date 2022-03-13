eb69986256bb77f79aa109ed8b8a251efa4b2b5d
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-03-13 23:04:58 +0000

    remove and redundant code and set get route for user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,8 +3,8 @@ const User = require('./../models/userModel');
 
 // find a user by its _id
 const getUser = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id, (error, result) => {
-        if (error) return res.status(404).json({ message: `${error}` });
+    User.findById(req.params.id, (error, result) => {
+        if (error) return res.status(400).json({ message: `${error}` });
 
         res.status(200).json(result);
     });
--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,19 @@ const express = require("express");
 require("colors");
 const { listen } = require("express/lib/application");
 const req = require("express/lib/request");
-const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
+const app = express();
+
 // database connection
-//require('./config/db')();
+require('./config/db')();
 
 // parse the incoming request and respost to json format
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
+app.use('/api/v1/users', require('./routes/userRoutes'));
 
 // use custom error handler 'errorHandler' middleware
 app.use(require('./middlewares/errorHandler'));
