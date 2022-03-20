ecf134ae8a4818824dfcec8ee4634887a3fae00e
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-03-20 22:13:26 +0000

    setup bid routes

---
+++ backend/routes/api/bidRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { getBid, setBid, deleteBid } = require('./../../controllers/bidController');
+
+router.post('/', setBid);
+router.route('/:id').get(getBid).delete(deleteBid);
+
+module.exports = router;
