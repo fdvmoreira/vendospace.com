97abcd91df78f77aa2b6543ecdc790fb02b99448
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-03-28 23:42:25 +0100

    Write notes for getBid function

--- backend/controllers/bidController.js
+++ backend/controllers/bidController.js
@@ -2,13 +2,16 @@ const asynHandler = require('express-async-handler');
 const mongoose = require('mongoose');
 const Bid = require('./../models/bidModel');
 
-/** fields
+/** bid fields **
+ * 
  * id
  * bidder
  * auction
  * price
  */
-// get bid
+
+// get bid - 
+// Which parameters will be used to get the bids info ?
 const getBid = asynHandler(async (req, res) => {
     Bid.findById(req.body.id, (err, doc) => {
         if (err) res.status(404).json({ message: `${err.message}` });
