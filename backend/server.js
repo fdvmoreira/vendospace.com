3798c74246c5757f0c2e83e0a644d2496f6c7c4d
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-04-18 23:16:28 +0100

    set APIs routes gateway

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(require('./routes/vendoSpaceRoutes'));
+
+// API routes
 app.use('/api/v1/users', require('./routes/api/v1/userRoutes'));
+app.use('/api/v1/abuses', require('./routes/api/v1/abuseRoutes'));
+app.use('/api/v1/accounts', require('./routes/api/v1/accountRoutes'));
+app.use('/api/v1/listings', require('./routes/api/v1/listingRoutes'));
+app.use('/api/v1/bids', require('./routes/api/v1/bidRoutes'));
+app.use('/api/v1/messages', require('./routes/api/v1/messageRoutes'));
+app.use('/api/v1/notifications', require('./routes/api/v1/notificationRoutes'));
+app.use('/api/v1/profiles', require('./routes/api/v1/profileRoutes'));
+app.use('/api/v1/spaces', require('./routes/api/v1/spaceRoutes'));
+app.use('/api/v1/user-notifications', require('./routes/api/v1/userNotificationRoutes'));
+
 
 // use custom error handler 'errorHandler' middleware
 app.use(require('./middlewares/errorHandler'));
