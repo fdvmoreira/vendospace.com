acb7f4c7f3f0edd7729ce280ab23b71156940123
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-05-09 21:00:13 +0100

    integrate bootstrap into nextjs app

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,6 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import Layout from '../components/layout';
 import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+
 }
