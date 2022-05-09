c56e129925c8497fab821b10532a1de728a210b4
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-05-09 13:31:48 +0100

    amend routes by adding slash in front of each route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,22 +3,22 @@ const passport = require('passport');
 const router = require('express').Router();
 
 // media routes
-router.get('auth/google', passport.authenticate('google'));
-router.get('auth/facebook', passport.authenticate('facebook'));
-router.get('auth/linkedin', passport.authenticate('linkedin'));
+router.get('/auth/google', passport.authenticate('google'));
+router.get('/auth/facebook', passport.authenticate('facebook'));
+router.get('/auth/linkedin', passport.authenticate('linkedin'));
 
 // social media callback 
-router.get('auth/google/callback', passport.authenticate('google',
+router.get('/auth/google/callback', passport.authenticate('google',
     { failureRedirect: '/login', successRedirect: '/' }), (req, res) => {
 
     });
 
-router.get('auth/facebook/callback', passport.authenticate('facebook',
+router.get('/auth/facebook/callback', passport.authenticate('facebook',
     { failureRedirect: '/login', successRedirect: '/' }), (req, res) => {
 
     });
 
-router.get('auth/linkedin/callback', passport.authenticate('linkedin',
+router.get('/auth/linkedin/callback', passport.authenticate('linkedin',
     { failureRedirect: '/login', successRedirect: '/' }), (req, res) => {
 
     });
