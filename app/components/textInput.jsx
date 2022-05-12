8353cd34ed99e0f4cf50d87da25e56f85cbe9d3a
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-05-13 00:51:24 +0100

    add value property to text input component

--- app/components/textInput.jsx
+++ app/components/textInput.jsx
@@ -1,7 +1,9 @@
-export default function TextInput({type, name, placeholder}){
+const TextInput = ({type, name, value, placeholder, required})=>{
     return(
         <>
-            <input type={type} name={name} placeholder={placeholder} className="form-control my-3"/>
+            <input type={type} name={name} value={value} placeholder={placeholder} required={required} className="form-control my-3"/>
         </>
     );
 }
+
+export default TextInput;
