fdd9afb30a1d98d5fc670166cfa90956e483fab4
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-05-28 23:41:11 +0100

    list adpace cards

--- app/pages/adspaces/index.jsx
+++ app/pages/adspaces/index.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import AdSpaceCard from "../../components/adSpaceCard";
 
 export default function AdSpace(props) {
-  return <div>List of Adspaces </div>;
+  const [data, data2] = props.data;
+  const cards = props.data.map((item, index) => {
+    return <AdSpaceCard data={item} />;
+  });
+
+  return (
+    <div div className='container d-flex justify-content-between mt-5'>
+      {
+        //<AdSpaceCard data={data} />
+        cards.map((item) => item)
+      }
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
   return {
-    props: {},
+    props: {
+      data: [
+        {
+          type: "Roadside",
+          address: "78 Jaslow Garlic",
+          dimension: { width: 7, height: 9 },
+          location: { latitude: 78, logitude: 41 },
+          images: ["/logo.jpeg", "/logo.jpeg"],
+        },
+        {
+          type: "Billboard",
+          address: "20  Jakare Cosl",
+          dimension: { width: 20, height: 12 },
+          location: { latitude: 8, logitude: 4 },
+          images: ["/logo.jpeg", "/logo.jpeg"],
+        },
+      ],
+    },
   };
 }
