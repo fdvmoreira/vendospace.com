0a76d553e5b30c262b70a5275d28e265c2d01798
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-07-13 23:01:53 +0100

    create the buiding structure of the new form for creating listing

--- app/pages/listings/create.jsx
+++ app/pages/listings/create.jsx
@@ -1,8 +1,39 @@
+import { useForm } from "react-hook-form";
+import { yup } from "yup";
+
 export default function Listing() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
     <div className='container'>
-      <h1 children='lead'>Create new Listing</h1>
+      <h1 className='lead'>Create new Listing</h1>
       <hr className='' />
+
+      {/** Ad space */}
+      <span>Ad Space</span>
+
+      {/** User ID */}
+      <span>User ID</span>
+
+      {/**  */}
+      <select name='' id=''>
+        <option value=''>Available</option>
+        <option value=''>Sold</option>
+        <option value=''>Rented</option>
+        <option value=''>Active</option>
+        <option value=''>Disabled</option>
+      </select>
+      {/**  */}
+
+      {/**  */}
+
+      {/**  */}
+
+      {/**  */}
     </div>
   );
 }
