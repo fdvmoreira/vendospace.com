b3b262765b0badaf49838093adb27bd47762f82b
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-07-20 23:12:12 +0100

    add image domains and environment variables to next config

--- app/next.config.js
+++ app/next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  env: {
+    HOSTNAME: "localhost",
+    PORT: 3000,
+
+    CLOUDINARY_API_KEY: "256417812456237",
+    CLOUDINARY_API_SECRET: "U9WovbWDP3koenUMQb2_B46g_Zk",
+    CLOUDINARY_CLOUD_NAME: "vendospace-com",
+    CLOUDINARY_UNAUTH_UPLOAD_URL: `https://api.cloudinary.com/v1_1/vendospace-com/auto/upload`,
+    CLOUDINARY_UNAUTH_UPLOAD_PRESET: "aici91ve",
+  },
   reactStrictMode: true,
 }
 
