b4dc98cf659161c7afb3d6982b028e35ab1d980e
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-07-21 20:57:44 +0100

    refactor space controller to return more info when endpoint is hit

--- app/server/controllers/spaceController.js
+++ app/server/controllers/spaceController.js
@@ -4,7 +4,7 @@ const Space = require('../models/spaceModel')
 // find space by ID
 const getSpace = asyncHandler(async (req, res) => {
     Space.findById(req.params.id, (err, doc) => {
-        if (err) res.status().json({ message: `${err.message}` });
+        if (err) return res.status().json({ success: false, message: `${err.message}` });
 
         res.status(200).json(doc);
     });
@@ -15,9 +15,9 @@ const setSpace = asyncHandler(async (req, res) => {
     const { type, location, dimension, imagesURL, address } = req.body;
 
     Space.create({ type, location, dimension, imagesURL, address }, (err, doc) => {
-        if (err) res.status(400).json({ message: `${err.message}` });
+        if (err) return res.status(400).json({ message: `${err.message}` });
 
-        res.status(201).json({ message: `Ad-Space created with ID: ${doc._id}`, id: doc._id });
+        res.status(201).json({ success: true, message: `Ad-Space created with ID: ${doc._id}`, id: doc._id });
     });
 });
 
@@ -27,9 +27,9 @@ const updateSpace = asyncHandler(async (req, res) => {
     const { doc } = req.body; // TODO - field to update
 
     Space.findByIdAndUpdate(id, { doc }, (err, doc) => {
-        if (err) res.status(400).json({ message: `${err.message}` });
+        if (err) res.status(400).json({ success: false, message: `${err.message}` });
 
-        res.status(200).json({ message: `Ad-Space ${doc} updated` });
+        res.status(200).json({ success: true, message: `Ad-Space ${doc} updated` });
     });
 });
 
@@ -39,9 +39,9 @@ const deleteSpace = asyncHandler(async (req, res) => {
     const { id } = req.body;
 
     Space.findByIdAndDelete(id, (err, doc) => {
-        if (err) res.status(400).json({ message: `${err.message}` });
+        if (err) res.status(400).json({ success: false, message: `${err.message}` });
 
-        res.status(200).json({ message: `Ad-Space deleted` });
+        res.status(200).json({ success: true, message: `Ad-Space deleted` });
     });
 });
 
