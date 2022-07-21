8e292118f2dd3d1940aa29fb265b6b542e18f537
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-07-21 14:15:31 +0100

    use useEffect to load bootstrap js

--- app/pages/_app.jsx
+++ app/pages/_app.jsx
@@ -1,14 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+// react toastifylibrary
+import "react-toastify/dist/ReactToastify.css";
+import Layout from "../components/layout";
+import "../styles/globals.css";
 
-import Layout from '../components/layout';
-import '../styles/globals.css';
+import { LoginContext } from "../context/loginContext";
+
+import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
-
+    <LoginContext>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </LoginContext>
+  );
 }
