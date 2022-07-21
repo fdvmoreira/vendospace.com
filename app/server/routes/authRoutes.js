6e139e61c2f7a906c3cb74e517a556101632cf53
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-07-21 21:13:14 +0100

    add failure message and disable session because auth uses jwt

--- app/server/routes/authRoutes.js
+++ app/server/routes/authRoutes.js
@@ -8,19 +8,50 @@ router.get('/auth/facebook', passport.authenticate('facebook'));
 router.get('/auth/linkedin', passport.authenticate('linkedin'));
 
 // social media callback 
+/** GOOGLE */
 router.get('/auth/google/callback', passport.authenticate('google',
-    { failureRedirect: '/login', successRedirect: '/' }), (req, res) => {
+    {
+        failureRedirect: '/login',
+        failureMessage: "Failed",
+        successRedirect: '/',
+        successMessage: "Success",
+        session: false
+    }, (...args) => {
+        console.log("PROFILE GOOGLE");
+        console.log(args[1]);
+    }));
 
-    });
+/**
+ * FACEBOOK
+ */
+router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    failureRedirect: '/login',
+    failureMessage: "Faile",
+    successRedirect: '/',
+    successMessage: "Success",
+    session: false,
+    scope: ['id', 'displayName', 'photos', 'email']
+}, (...args) => {
 
-router.get('/auth/facebook/callback', passport.authenticate('facebook',
-    { failureRedirect: '/login', successRedirect: '/' }), (req, res) => {
+    console.log("PROFILE LINKEDIN:");
+    console.log(args[1]);
+}));
 
-    });
-
+/** LINKEDIN */
 router.get('/auth/linkedin/callback', passport.authenticate('linkedin',
-    { failureRedirect: '/login', successRedirect: '/' }), (req, res) => {
+    {
+        failureRedirect: '/login',
+        failureMessage: "Failed",
+        successRedirect: '/',
+        successMessage: "Success",
+        session: false,
+        scope: ['r_emailaddress', 'r_liteprofile']
+    }, (...args) => {
 
-    });
+        console.log("PROFILE LINKEDIN:");
+        console.log(args[1]);
+
+        // res.status(200).json(req.body);
+    }));
 
 module.exports = router;
