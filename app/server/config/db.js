5e08fe00b577020537a9cd5539e158e1cbd0a388
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-07-21 20:22:13 +0100

    add port number to log message

--- app/server/config/db.js
+++ app/server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Connected to ${conn.connection.host} ${conn.connection.port}`.blue.bold.underline);
+        console.log(`Connected to ${conn.connection.host} on port ${conn.connection.port}`.blue.bold.underline);
     } catch (error) {
         console.error(`Error has occured: ${error}`.red.bold.underline);
     }
