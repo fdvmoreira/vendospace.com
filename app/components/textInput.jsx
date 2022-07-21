cf7c72903d7a3a7e1f230e4f695ec959849024b2
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-07-21 20:24:58 +0100

    create custom text input component

--- app/components/textInput.jsx
+++ app/components/textInput.jsx
@@ -1,9 +1,16 @@
-const TextInput = ({type, name, value, placeholder, required})=>{
-    return(
-        <>
-            <input type={type} name={name} value={value} placeholder={placeholder} required={required} className="form-control my-3"/>
-        </>
-    );
-}
+const TextInput = ({ type, name, value, placeholder, required }) => {
+  return (
+    <>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        required={required}
+        className='form-control my-3'
+      />
+    </>
+  );
+};
 
 export default TextInput;
