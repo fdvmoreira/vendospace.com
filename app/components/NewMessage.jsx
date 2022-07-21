a8bcdc4b7dd4361bf0a61ae79585eaf9bb8a06d3
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-07-21 20:33:03 +0100

    create messaging modal

---
+++ app/components/NewMessage.jsx
@@ -0,0 +1,98 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+export default function NewMessage({ recipient, subject }) {
+  const schema = yup.object().shape({
+    recipient: yup.string().required(),
+    subject: yup.string().required(),
+    message: yup.string().max(200).required(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  return (
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <div
+        className='modal fade m-auto'
+        id='messageModal'
+        tabIndex='-1'
+        role='dialog'
+        aria-hidden='true'>
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h5 className='modal-title'>New Message</h5>
+              {/** modal close button */}
+              <button
+                type='button'
+                className='btn close'
+                data-bs-dismiss='modal'
+                aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+              </button>
+            </div>
+            <div className='modal-body'></div>
+            {/** recipient */}
+            <div className='m-1'>
+              <input
+                type='text'
+                className='form-control'
+                placeholder='Recipient'
+                {...register("recipient")}
+              />
+              {/** recipient error check */}
+              {errors.recipient?.message && (
+                <span className='text-danger'>{errors.recipient.message}</span>
+              )}
+            </div>
+            {/** subject */}
+            <div className='m-1'>
+              <input
+                type='text'
+                className='form-control'
+                placeholder='Subject'
+                {...register("subject")}
+              />
+              {/** subject error check */}
+              {errors.subject?.message && (
+                <span className='text-danger'>{errors.subject.message}</span>
+              )}
+            </div>
+            {/** message */}
+            <div className='form-outline m-1'>
+              <textarea
+                id='message'
+                placeholder='Message'
+                className='form-control'
+                rows='4'
+                {...register("message")}
+              />
+              {/** message error check */}
+              {errors.message?.message && (
+                <span className='text-danger'>{errors.message.message}</span>
+              )}
+            </div>
+            <div className='modal-footer'>
+              <button type='button' className='btn' data-bs-dismiss='modal'>
+                {" "}
+                Cancel
+              </button>
+              <input
+                type='submit'
+                className='btn btn-success'
+                value='Send Message'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
