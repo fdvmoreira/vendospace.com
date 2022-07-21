24351d0d00fcf1c00134a5fb139ad5302c28e219
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-07-21 20:48:31 +0100

    change login context to take user and status field

--- app/context/loginContext.js
+++ app/context/loginContext.js
@@ -3,25 +3,22 @@ import { createContext, useState, useContext } from 'react';
 // create context object
 const loginContext = createContext();
 
-// create context component
+// create context provider component
 export function LoginContext(props) {
-
-    const [login, setLogin] = useState(true);
-
+    const [login, setLogin] = useState({
+        userId: undefined,
+        loggedIn: false
+    });
     // update login context
-    function updateLogin(status) {
-        setLogin(status);
+    function updateLogin(user) {
+        setLogin(user);
     }
-
     return <loginContext.Provider value={[login, updateLogin]} {...props} />
 }
 
 // create custom hook
-
 export function useLogin() {
-
     const context = useContext(loginContext)
     if (!context) throw new Error('Login Context undefined');
     return context;
-
 }
