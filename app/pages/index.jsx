684f2b08c12d334e4b18c35a78d35d915b995906
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-07-21 20:21:43 +0100

    listing the auctions and listings in the database

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,177 +1,64 @@
-import React from "react";
-import TextInput from "../components/textInput";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import AuctionCard from "../components/auctionCard";
+import ListingCard from "../components/listingCard";
 
-const Index = (props) => {
-  const [currentValue, setCurrentValue] = useState(0);
-  const [otherType, setOtherType] = useState({ display: "none" });
+const AUCTIONS_API_URL = "/api/v1/auctions";
+const LISTINGS_API_URL = "/api/v1/listings";
 
-  const handleDimensionRangeChange = (event) => {
-    setCurrentValue(event.target.value);
-  };
+export default (props) => {
+  let [auctions, setAuctions] = useState([]);
+  let [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    /** Auctions */
+    fetch(AUCTIONS_API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setAuctions(data);
+      })
+      .catch((err) => console.error(err));
+
+    /** Listings */
+    fetch(LISTINGS_API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setListings(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <div className='container'>
-      <form onSubmit={handleSubmit}>
-        <div className='input-group'>
-          <input
-            type='search'
-            className='form-control rounded '
-            placeholder='Search'
-            aria-label='Search'
-          />
-          <button type='button' className='btn btn-outline-primary'>
-            search
-          </button>
-        </div>
-      </form>
+      <hr />
+      <p className='lead'>welcome</p>
+      <p>{props.data}</p>
+      <ul className='list-group'>
+        {auctions.map((auction) => {
+          return (
+            <li key={auction._id} className='list-group-item list-group-flush'>
+              <AuctionCard auction={auction} />
+            </li>
+          );
+        })}
 
-      <div className='row d-flex'>
-        <aside className='border flex d-flex aside col'>
-          <form onSubmit={handleFilter}>
-            <h6>Filter</h6>
-            <div className='form-check'>
-              <input
-                className='form-check-input'
-                type='checkbox'
-                name='auction'
-                id='auction'
-              />
-              <label htmlFor='auction' className='form-check-label'>
-                {" "}
-                Auction{" "}
-              </label>
-            </div>
-
-            <div className='form-check mb-2'>
-              <input
-                className='form-check-input'
-                type='checkbox'
-                name='standard'
-                id='standard'
-              />
-              <label htmlFor='standard' className='form-check-label'>
-                {" "}
-                Standard{" "}
-              </label>
-            </div>
-
-            <div className='mb-2'>
-              <select
-                name=''
-                defaultValue={"All"}
-                onChange={(event) => {
-                  if (event.target.value === "other") {
-                    setOtherType({ display: "block" });
-                    console.log(event.target.value);
-                    return;
-                  }
-                  setOtherType({ display: "none" });
-                }}
-                id='medium'
-                className='form-select'
-                aria-label='Default select example'
-                required>
-                <option value='all'>All</option>
-                <option value='billboard'>Billboard</option>
-                <option value='bridge'>Bridge</option>
-                <option value='bus'>Bus</option>
-                <option value='digital'>Digital Transit</option>
-                <option value='guerrila'>Guerrila</option>
-                <option value='lampPost'>Lamp Post</option>
-                <option value='outdoor'>Outdoors</option>
-                <option value='pointOfSale'>Point of Sale</option>
-                <option value='poster'>Poster</option>
-                <option value='rail'>Rail</option>
-                <option value='shelter'>Shelter</option>
-                <option value='stunt'>Stunt</option>
-                <option value='transit'>Transit</option>
-                <option value='wall'>Wall</option>
-                <option value='other'>Other</option>
-              </select>
-            </div>
-
-            {/**
-             * Other types
-             */}
-            <input
-              type='text'
-              placeholder='Type other ad space here ...'
-              name='typeDescription'
-              id='typeDescription'
-              style={otherType}
-              className='form-control mb-2'
-            />
-
-            {/** Price */}
-            <fieldset className='text-muted'>
-              <legend>Price</legend>
-              <div className='input-group mb-2'>
-                <input
-                  type='text'
-                  inputMode='number'
-                  maxLength={7}
-                  size={7}
-                  placeholder='price min'
-                  className='form-control me-1'
-                />
-                <input
-                  type='text'
-                  inputMode='number'
-                  maxLength={7}
-                  size={7}
-                  placeholder='price max'
-                  className='form-control ms-1'
-                />
-              </div>
-            </fieldset>
-
-            {/** size */}
-            <fieldset className='text-muted'>
-              <legend>Size</legend>
-              <div className='input-group mb-2 g-3'>
-                <input
-                  type='text'
-                  placeholder='width'
-                  className='form-control me-1'
-                />
-
-                <input
-                  type='text'
-                  placeholder='height'
-                  className='form-control ms-1 me-1'
-                />
-
-                <select className='form-select ms-1' defaultValue={""} required>
-                  <option value='' disabled>
-                    unit
-                  </option>
-                  <option value='centimeter'>cm</option>
-                  <option value='foot'>ft</option>
-                </select>
-              </div>
-            </fieldset>
-
-            {/** Apply button */}
-            <input
-              type='submit'
-              value='Apply'
-              className='btn btn-outline-success mb-3'
-            />
-          </form>
-        </aside>
-
-        <main className='col border'>list of posts</main>
-      </div>
+        {listings.map((listing) => {
+          return (
+            <li key={listing._id} className='list-group-item list-group-flush'>
+              <ListingCard listing={listing} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  console.log(event.target);
-};
-
-const handleFilter = (event) => {};
-
-export default Index;
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      data: "my testing data",
+    },
+  };
+}
