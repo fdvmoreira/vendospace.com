24e0bf96299a741c3596736568d3fe6d377b15ab
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-07-26 19:10:08 +0100

    Revert "Fixes #6"
    This reverts commit 2e2466d037f04220cb04d7fd38e71c5274e29c5a.

--- app/pages/auctions/create.jsx
+++ app/pages/auctions/create.jsx
@@ -19,18 +19,12 @@ export default function Auction({ data }) {
     start: yup
       .date()
       .required()
-      .test("start-after-now", "This date has passed", (startdate, context) => {
-        return startdate.getTime() > Date.now();
-      })
+      .default(() => Date())
       .typeError("Start date and time are required"),
     end: yup
       .date()
       .required()
-      .test(
-        "is-after-start",
-        "End data is before start date",
-        (value, context) => value.getTime() > context.parent.start.getTime(),
-      )
+      .default(() => Date())
       .typeError("End date and time are required"),
     initialPrice: yup
       .number()
