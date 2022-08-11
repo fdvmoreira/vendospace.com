5470f131f218cc995b89ebf1f0d287fa40facbc4
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-08-11 23:21:05 +0100

    validate the bidding data sent by user

--- app/server/routes/api/v1/bidRoutes.js
+++ app/server/routes/api/v1/bidRoutes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const { body, validationResult } = require('express-validator');
 const { getBid, setBid, deleteBid } = require('../../../controllers/bidController');
 
-router.post('/', setBid);
+// TODO: make sure that the bid is bigger than the current biggest bid before creating a new one
+router.post('/',
+    body("bidder").isHexadecimal().isByteLength(12),
+    body("auction").isHexadecimal().isByteLength(12),
+    body("price").isDecimal(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (errors.length > 0) { return res.status(400).json({ success: false, message: errors[0].msg }); }
+        next();
+    }, setBid);
+
 router.route('/:id').get(getBid).delete(deleteBid);
 
 module.exports = router;
