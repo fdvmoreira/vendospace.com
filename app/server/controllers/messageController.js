31944d2f0a9476c40aa78b68366cc6ffca704525
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-08-15 21:24:36 +0100

    add success field to determine if the returning data from server is success

--- app/server/controllers/messageController.js
+++ app/server/controllers/messageController.js
@@ -4,7 +4,7 @@ const Message = require('./../models/messageModel');
 // get Message by id
 const getMessage = asyncHandler(async (req, res) => {
     Message.findById(req.params.id, (err, doc) => {
-        if (err) res.status(400).json({ Error: `${err.message}` });
+        if (err) res.status(400).json({ success: false, message: `${err.message}` });
 
         res.status(200).json(doc);
     });
@@ -14,9 +14,9 @@ const getMessage = asyncHandler(async (req, res) => {
 const setMessage = asyncHandler(async (req, res) => {
     const { sender, receiver, text } = req.body;
     Message.create({ sender, receiver, text }, (err, doc) => {
-        if (err) res.status(400).json({ Error: `${err.message}` });
+        if (err) res.status(400).json({ success: false, message: `${err.message}` });
 
-        res.status(201).json({ message: `Message ${doc.id} created` });
+        res.status(201).json({ success: true, message: `Message ${doc.id} created` });
     });
 });
 
@@ -24,18 +24,18 @@ const setMessage = asyncHandler(async (req, res) => {
 const updateMessage = asyncHandler(async (req, res) => {
     const body = req.body;
     Message.findByIdAndUpdate(req.params.id, { body }, (err, doc) => {
-        if (err) res.status(400).json({ Error: `${err.message}` });
+        if (err) res.status(400).json({ success: false, message: `${err.message}` });
 
-        res.status(201).json({ message: `Message ${doc._id} updated` });
+        res.status(201).json({ success: true, message: `Message ${doc._id} updated` });
     });
 });
 
 // delete Message
 const deleteMessage = asyncHandler(async (req, res) => {
     Message.findByIdAndDelete(req.params.id, (err, doc) => {
-        if (err) res.status(400).json({ Error: `${err.message}` });
+        if (err) res.status(400).json({ success: false, message: `${err.message}` });
 
-        res.status().json({ message: `Message ${doc?.id} deleted` });
+        res.status().json({ success: true, message: `Message ${doc?.id} deleted` });
     });
 });
 module.exports = {
