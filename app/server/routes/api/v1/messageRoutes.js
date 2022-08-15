51a26a900577d12ac5373837825602e394023187
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-08-15 21:52:21 +0100

    validate the message before saving to db

--- app/server/routes/api/v1/messageRoutes.js
+++ app/server/routes/api/v1/messageRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const asynHandler = require("express-async-handler");
 const router = express.Router();
 const { getMessage, setMessage, deleteMessage } = require('../../../controllers/messageController');
@@ -6,11 +7,22 @@ const Message = require('../../../models/messageModel');
 
 router.route("/").get(asynHandler(async (req, res) => { // todo - remove this route
     Message.find((err, doc) => {
-        if (err) res.status(400).json({ error: `You've messed up: ${err.message}` });
+        if (err) res.status(400).json({ success: false, message: `You've messed up: ${err.message}` });
         res.status(200).json(doc);
     });
 
-})).post(setMessage);
+})).post(
+    //TODO: sanitize message 
+    body("recipient").isHexadecimal().isByteLength(12),
+    body("subject").isString().isLength({ max: 50 }).notEmpty(),
+    body("message").isString().isLength({ max: 200 }).notEmpty(),
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({
+            success: false, message: errors.array()
+        }
+    },
+    setMessage);
 
 // todo -remove delete route because the database must keep history 
 // only deactive the account when the use requests delete account
