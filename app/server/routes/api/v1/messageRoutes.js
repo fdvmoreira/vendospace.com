b03d546c4e56781941505402f4db08f1476bb38d
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-08-30 13:31:27 +0100

    sanitise the input data and check for errors on new middleware

--- app/server/routes/api/v1/messageRoutes.js
+++ app/server/routes/api/v1/messageRoutes.js
@@ -12,19 +12,22 @@ router.route("/").get(asynHandler(async (req, res) => { // todo - remove this ro
     });
 
 })).post(
-    //TODO: sanitize message 
-    body("recipient").isHexadecimal().isByteLength(12),
-    body("subject").isString().isLength({ max: 50 }).notEmpty(),
-    body("message").isString().isLength({ max: 200 }).notEmpty(),
-    (req, res) => {
+    body("from").trim().isMongoId(),
+    body("to").trim().isMongoId(),
+    body("subject").trim().notEmpty().isString().isLength({ max: 50 }).escape(),
+    body("text").trim().notEmpty().isString().isLength({ max: 200 }).escape(),
+    asynHandler(async (req, res, next) => {
         const errors = validationResult(req);
-        if (!errors.isEmpty()) return res.status(400).json({
-            success: false, message: errors.array(),
-        })
-    },
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false, message: `${errors.array({ onlyFirstError: true })[0]}`,
+            });
+        }
+        next();
+    }),
     setMessage);
 
-// todo -remove delete route because the database must keep history 
+// TODO: Handle-remove delete route because the database must keep history 
 // only deactive the account when the use requests delete account
 router.route('/:id').get(getMessage).delete(deleteMessage);
 
