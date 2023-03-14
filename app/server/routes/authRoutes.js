2ddbbc24fea966588b2312afa2313ebfdcb0ad29
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-03-14 23:54:44 +0000

    reformat the document and relabel the log message

--- app/server/routes/authRoutes.js
+++ app/server/routes/authRoutes.js
@@ -10,46 +10,46 @@ router.get('/auth/linkedin', passport.authenticate('linkedin'));
 // social media callback 
 /** GOOGLE */
 router.get('/auth/google/callback', passport.authenticate('google',
-    {
-        session: false
-    }
+  {
+    session: false
+  }
 ), (req, res) => {
-    if (!req.user) return res.status(204).json({ message: "Unauthorized access" })
-    res.status(200).json({ message: "End of Google's ops", user: req?.user })
-    //TODO send the user the front end so it can used for auth
+  if (!req.user) return res.status(204).json({ message: "Unauthorized access" })
+  res.status(200).json({ message: "End of Google's ops", user: req?.user })
+  //TODO send the user the front end so it can used for auth
 });
 
 /**
  * FACEBOOK
  */
 router.get('/auth/facebook/callback', passport.authenticate('facebook', {
-    failureRedirect: '/login',
-    failureMessage: "Faile",
-    successRedirect: '/',
-    successMessage: "Success",
-    session: false,
-    scope: ['id', 'displayName', 'photos', 'email']
+  failureRedirect: '/login',
+  failureMessage: "Faile",
+  successRedirect: '/',
+  successMessage: "Success",
+  session: false,
+  scope: ['id', 'displayName', 'photos', 'email']
 }, (...args) => {
 
-    console.log("PROFILE LINKEDIN:");
-    console.log(args[1]);
+  console.log("PROFILE FACEBOOK:");
+  console.log(args[1]);
 }));
 
 /** LINKEDIN */
 router.get('/auth/linkedin/callback', passport.authenticate('linkedin',
-    {
-        failureRedirect: '/login',
-        failureMessage: "Failed",
-        successRedirect: '/',
-        successMessage: "Success",
-        session: false,
-        scope: ['r_emailaddress', 'r_liteprofile']
-    }, (...args) => {
+  {
+    failureRedirect: '/login',
+    failureMessage: "Failed",
+    successRedirect: '/',
+    successMessage: "Success",
+    session: false,
+    scope: ['r_emailaddress', 'r_liteprofile']
+  }, (...args) => {
 
-        console.log("PROFILE LINKEDIN:");
-        console.log(args[1]);
+    console.log("PROFILE LINKEDIN:");
+    console.log(args[1]);
 
-        // res.status(200).json(req.body);
-    }));
+    // res.status(200).json(req.body);
+  }));
 
 module.exports = router;
