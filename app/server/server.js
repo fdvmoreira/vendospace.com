be1a42cab6a5a7f7c977c21f3806947e720d1386
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-02 00:24:27 +0100

    reformat the spacing to 2 instead of 4

--- app/server/server.js
+++ app/server/server.js
@@ -15,48 +15,48 @@ const handler = nextApp.getRequestHandler();
 
 nextApp.prepare().then(async () => {
 
-    const app = express();
+  const app = express();
 
-    // database connection
-    require('./config/db')();
+  // database connection
+  require('./config/db')();
 
-    // passport auth strategies
-    require("./config/passport.config");
-
-
-    // parse the incoming request and respost to json format
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    // protec the application against well-known vulnerabilities
-    // disable x-powered-by header to obfuscate the server
-    // app.disable("x-powered-by");
-    app.use(helmet.hidePoweredBy());
+  // passport auth strategies
+  require("./config/passport.config");
 
 
-    // vendospace routes
-    app.use(require('./routes/vendoSpaceRoutes'));
-    // auth routes
-    app.use(require("./routes/authRoutes"));
+  // parse the incoming request and respost to json format
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  // protec the application against well-known vulnerabilities
+  // disable x-powered-by header to obfuscate the server
+  // app.disable("x-powered-by");
+  app.use(helmet.hidePoweredBy());
 
-    // API routes
-    app.use('/api/v1/users', require('./routes/api/v1/userRoutes'));
-    app.use('/api/v1/abuses', require('./routes/api/v1/abuseRoutes'));
-    app.use('/api/v1/accounts', require('./routes/api/v1/accountRoutes'));
-    app.use('/api/v1/auctions', require('./routes/api/v1/auctionRoutes'));
-    app.use('/api/v1/listings', require('./routes/api/v1/listingRoutes'));
-    app.use('/api/v1/bids', require('./routes/api/v1/bidRoutes'));
-    app.use('/api/v1/messages', require('./routes/api/v1/messageRoutes'));
-    app.use('/api/v1/notifications', require('./routes/api/v1/notificationRoutes'));
-    app.use('/api/v1/profiles', require('./routes/api/v1/profileRoutes'));
-    app.use('/api/v1/spaces', require('./routes/api/v1/spaceRoutes'));
-    app.use('/api/v1/user-notifications', require('./routes/api/v1/userNotificationRoutes'));
 
-    //nextjs
-    app.all("*", (req, res) => handler(req, res));
+  // vendospace routes
+  app.use(require('./routes/vendoSpaceRoutes'));
+  // auth routes
+  app.use(require("./routes/authRoutes"));
 
-    // use custom error handler 'errorHandler' middleware
-    app.use(require('./middlewares/errorHandler'));
+  // API routes
+  app.use('/api/v1/users', require('./routes/api/v1/userRoutes'));
+  app.use('/api/v1/abuses', require('./routes/api/v1/abuseRoutes'));
+  app.use('/api/v1/accounts', require('./routes/api/v1/accountRoutes'));
+  app.use('/api/v1/auctions', require('./routes/api/v1/auctionRoutes'));
+  app.use('/api/v1/listings', require('./routes/api/v1/listingRoutes'));
+  app.use('/api/v1/bids', require('./routes/api/v1/bidRoutes'));
+  app.use('/api/v1/messages', require('./routes/api/v1/messageRoutes'));
+  app.use('/api/v1/notifications', require('./routes/api/v1/notificationRoutes'));
+  app.use('/api/v1/profiles', require('./routes/api/v1/profileRoutes'));
+  app.use('/api/v1/spaces', require('./routes/api/v1/spaceRoutes'));
+  app.use('/api/v1/user-notifications', require('./routes/api/v1/userNotificationRoutes'));
 
-    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`.bold.cyan.underline));
+  //nextjs
+  app.all("*", (req, res) => handler(req, res));
+
+  // use custom error handler 'errorHandler' middleware
+  app.use(require('./middlewares/errorHandler'));
+
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`.bold.cyan.underline));
 
 }).catch((err) => console.err);
