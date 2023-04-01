44b3f81634855661899d04b4a37c31855f807e25
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-02 00:23:03 +0100

    fix import statement

--- app/server/authStrategies/facebookStrategy.js
+++ app/server/authStrategies/facebookStrategy.js
@@ -1,18 +1,18 @@
-const User = require("../../models/userModel");
+const User = require("../models/userModel");
 const FacebookStrategy = require("passport-facebook").Strategy;
 const DOMAIN = process.env.DOMAIN + ":" + process.env.PORT;
 
 const facebookStrategy = new FacebookStrategy({
-    clientID: process.env.FACEBOOK_CLIENT_ID,
-    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    callbackURL: `${DOMAIN}/auth/facebook/callback`,
-    profileFields: ['id', 'displayName', 'photos', 'email'],
+  clientID: process.env.FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  callbackURL: `${DOMAIN}/auth/facebook/callback`,
+  profileFields: ['id', 'displayName', 'photos', 'email'],
 }, (accessToken, refreshToken, profile, done) => {
-    User.findById({ id: profile.id }, (err, user) => {
-        if (err) return done(err, false);
-        if (!user) return done(null, false);
-        return done(err, profile);
-    });
+  User.findById({ id: profile.id }, (err, user) => {
+    if (err) return done(err, false);
+    if (!user) return done(null, false);
+    return done(err, profile);
+  });
 });
 
 module.exports = facebookStrategy;
