59f5c26edc472d0bc31ba6e19d7cb03f9364b905
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-02 17:48:35 +0100

    write unit tests to functions that extract referer header and request object

---
+++ app/tests/authroutes.test.js
@@ -0,0 +1,42 @@
+const { getAuthType } = require("../server/config/auth.type");
+const { getLastWordFromPath, extractAuthTypeMiddleware } = require("../server/routes/authRoutes");
+
+describe("Get Last word from a URL", () => {
+
+  it("should throw an Exception", () => {
+    expect(getLastWordFromPath()).toBe.undefined;
+  });
+
+  it("should return undefined it URL is empty", () => {
+    const URL = "http://localhost";
+    expect(getLastWordFromPath(URL)).toBe.undefined;
+  });
+
+  it("should return last word from the path", () => {
+    const URL = "http://example.com/users";
+    expect(getLastWordFromPath(URL)).toMatch(/users/);
+  });
+});
+
+describe("Extract referer header", function () {
+
+  it("should set authtype variable to referer header's last word", function () {
+    let request = { headers: { referer: "http://localhost:3000/register" } };
+    extractAuthTypeMiddleware(request, null, function (err) { });
+    let authType = getAuthType();
+    expect(['SIGNUP', 'REGISTER']).toContain(authType);
+
+    request = { headers: { referer: "http://localhost:3000/login" } };
+    extractAuthTypeMiddleware(request, null, function (err) { });
+    authType = getAuthType();
+    expect(['SIGNIN', 'LOGIN']).toContain(authType);
+  });
+
+  it("should set authType to 'DEFAULT'", function () {
+    let request = { headers: { referer: "http://localhost:3000/AAAA" } };
+    extractAuthTypeMiddleware(request, null, function (err) { });
+    let authType = getAuthType();
+    expect(authType).toMatch(/DEFAULT/);
+  });
+
+});
