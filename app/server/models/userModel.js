a80b32b2c923c6681dcf9f813d74fc0a2fc0f8b5
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-04 23:13:47 +0100

    fixes linkedin not supported in the current model #48

--- app/server/models/userModel.js
+++ app/server/models/userModel.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    passwordHash: String,
-    signUpMethod: {
-        type: String,
-        required: true,
-        enum: {
-            values: ['google', 'facebook', 'linkedIn', 'microsoft', 'Email'], // todo - signup methods here
-            message: '{VALUE} not supported here'
-        }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  passwordHash: String,
+  signUpMethod: {
+    type: String,
+    required: true,
+    enum: {
+      values: ['google', 'facebook', 'linkedin', 'email'],
+      message: '{VALUE} not supported here'
     }
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
