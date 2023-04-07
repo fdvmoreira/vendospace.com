43ab84d738e3a517cfcacc2f8a6f940bddcbe3c8
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-08 00:11:55 +0100

    move password hashing logic to user model

--- app/server/controllers/userController.js
+++ app/server/controllers/userController.js
@@ -5,60 +5,49 @@ const User = require('../models/userModel');
 
 // find a user by its _id
 const getUser = asyncHandler(async (req, res) => {
-    User.findById(req.params.id, (error, doc) => {
-        if (error) {
-            res.status(404).json({ error: `${error.message}` });
-        }
-        res.status(200).json(doc);
-    });
+  User.findById(req.params.id, (error, doc) => {
+    if (error) {
+      res.status(404).json({ error: `${error.message}` });
+    }
+    res.status(200).json(doc);
+  });
 });
 
 // create new user
 const setUser = asyncHandler(async (req, res) => {
-    let { name, email, password, 'signup-method': signUpMethod } = req.body;
+  let { name, email, password: passwordHash, 'signup-method': signUpMethod } = req.body;
 
-    HASH_ROUND = 7;
-    let passwordHash = "";
+  User.create({
+    name,
+    email,
+    passwordHash,
+    signUpMethod
+  }, (error, doc) => {
+    if (error) res.status(404).json({ error: `${error.message}` }); //throw new Error('Could not create user');
 
-    if (signUpMethod == "Email") {
-        // create password hash
-        passwordHash = await bcrypt.hash(password, HASH_ROUND);
-        if (!passwordHash) {
-            res.status(400).json({ message: `${passwordHash}` });
-        }
-    }
-
-    User.create({
-        name,
-        email,
-        passwordHash,
-        signUpMethod
-    }, (error, doc) => {
-        if (error) res.status(404).json({ error: `${error.message}` }); //throw new Error('Could not create user');
-
-        res.status(201).json({ message: ` User id ${doc.id} created` });
-    });
+    res.status(201).json({ message: ` User id ${doc.id} created` });
+  });
 });
 
 // delete user
 const deleteUser = asyncHandler(async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    User.findByIdAndDelete(id, (error, doc) => {
+  User.findByIdAndDelete(id, (error, doc) => {
 
-        if (error) return res.status(400).json({ error: `${error.message}` });
+    if (error) return res.status(400).json({ error: `${error.message}` });
 
-        if (!doc) return res.status(404).json({ message: `${id} not found.` });
+    if (!doc) return res.status(404).json({ message: `${id} not found.` });
 
-        res.status(200).json({
-            message: `User ${doc?._id} removed.`, doc: `${1 + 1}`
-        });
+    res.status(200).json({
+      message: `User ${doc?._id} removed.`, doc: `${1 + 1}`
     });
+  });
 });
 
 module.exports = {
-    getUser,
-    setUser,
-    // updateUser,
-    deleteUser
+  getUser,
+  setUser,
+  // updateUser,
+  deleteUser
 };
