192a687c47a4e0424f84c7208d1ddccce8ede7fa
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-07 22:57:32 +0100

    create isPasswpordValid method to check if the password is valid

--- app/server/models/userModel.js
+++ app/server/models/userModel.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   name: {
@@ -20,5 +21,12 @@ const userSchema = new Schema({
     }
   }
 }, { timestamps: true });
+
+userSchema.methods.isPasswordValid = function (password) {
+  bcrypt.compare(password, this.passwordHash, (err, same) => {
+    if (err) return false;
+    if (same) return true;
+  });
+}
 
 module.exports = model('User', userSchema);
