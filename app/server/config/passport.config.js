df1780bddcc8004fbcda664121eb0991600909b6
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-08 02:49:34 +0100

    update: register local strategy to passport for use with login and signup routes

--- app/server/config/passport.config.js
+++ app/server/config/passport.config.js
@@ -4,8 +4,10 @@ const jwtStrategy = require("../authStrategies/jwtStrategy")
 const googleStrategy = require("../authStrategies/googleStrategy");
 const facebookStrategy = require("../authStrategies/facebookStrategy");
 const linkedInStrategy = require("../authStrategies/linkedInStrategy");
+const localStrategy = require("../authStrategies/localStrategy");
 
 passport.use(jwtStrategy);
 passport.use(linkedInStrategy);
 passport.use(googleStrategy);
 passport.use(facebookStrategy);
+passport.use(localStrategy);
