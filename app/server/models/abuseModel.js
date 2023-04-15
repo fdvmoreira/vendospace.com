3058c67de4fa249b6abc6772313cc88e799e9656
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-15 23:31:32 +0100

    fix:  remove typo and change the type to any document type

--- app/server/models/abuseModel.js
+++ app/server/models/abuseModel.js
@@ -1,30 +1,30 @@
 const mongoose = require('mongoose');
 
 const abuseSchema = new mongoose.Schema({
-    itemReportedId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Item' // todo - modify to link to the correct Object
-    },
-    createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    },
-    userReported: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    },
-    abuseType: {
-        type: String,
-        required: true,
-        enum: {
-            values: ['Other', 'Sexual', 'Hateful', 'Violent', 'Harrasment', 'Harmful', 'Child', 'Terrorism', 'Spam'],
-            message: '{VALUE} is not supported'
-        }
-    },
-    message: String
+  itemReportedId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    // ref: 'Item' // todo - modify to link to the correct Object
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  userReported: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  abuseType: {
+    type: String,
+    required: true,
+    enum: {
+      values: ['Other', 'Sexual', 'Hateful', 'Violent', 'Harassment', 'Harmful', 'Child', 'Terrorism', 'Spam', 'Scam'],
+      message: '{VALUE} is not supported'
+    }
+  },
+  message: String
 
 }, { timestamps: true });
 
