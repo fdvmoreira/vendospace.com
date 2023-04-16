2dcc38b803bf04c9a6be150cc355129f6c9888bc
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-16 19:28:00 +0100

    feat: add message reading card

---
+++ app/components/profile/message/MessageCard.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../../../context/authContext";
+const USERS_API = '/api/v1/users';
+
+const MessageCard = ({from, subject, message, createdAt}) =>{
+  const [name, setName] = useState();
+  const [auth,_] = useAuth();
+
+  useEffect(()=>{
+    fetch(`${USERS_API}/${from}/name`, {
+      headers:{
+        'Content-Type': 'application/json',
+        'Authorization':`Bearer ${auth.token}`,
+      },
+    })
+    .then(res => res.json())
+    .then(data => setName(data?.data?.name))
+    .catch(console.error);
+  },[])
+
+  return (
+    <div className="card my-2">
+    <div className="card-header">
+      <span className="text-muted">From: {(name)??"..."}</span>
+      <p className="fw-bold">{subject.toUpperCase()}</p>
+      </div>
+    <div className="card-body">{message}</div>
+    <div className="card-footer text-muted">{new Date(createdAt).toLocaleString('en-GB')}</div>
+  </div>
+  )
+}
+
+export default MessageCard;
