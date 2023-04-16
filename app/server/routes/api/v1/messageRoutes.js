559884dfc4e9956808b5f307cd322ff1cb3ab28e
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-16 18:28:58 +0100

    feat: add check for authentication before saving to db

--- app/server/routes/api/v1/messageRoutes.js
+++ app/server/routes/api/v1/messageRoutes.js
@@ -1,34 +1,42 @@
-const express = require('express');
+const router = require('express').Router();
 const { body, validationResult } = require('express-validator');
-const asynHandler = require("express-async-handler");
-const router = express.Router();
-const { getMessage, setMessage, deleteMessage } = require('../../../controllers/messageController');
+const asyncHandler = require("express-async-handler");
+const {
+  getMessage,
+  setMessage,
+  deleteMessage
+} = require('../../../controllers/messageController');
 const Message = require('../../../models/messageModel');
+const passport = require('passport');
+const authenticationCheck = require('../../../middlewares/auth/authenticationCheck');
+const ownershipCheck = require('../../../middlewares/auth/ownershipCheck');
 
-router.route("/").get(asynHandler(async (req, res) => { // todo - remove this route
-    Message.find((err, doc) => {
-        if (err) res.status(400).json({ success: false, message: `You've messed up: ${err.message}` });
-        res.status(200).json(doc);
-    });
+router.route("/").get(asyncHandler(async (req, res) => { // todo - remove this route
+  Message.find((err, docs) => {
+    if (err) return res.status(400).json({ success: false, message: `You've messed up: ${err.message}` });
+    res.status(200).json({ success: true, message: `Found ${docs?.length} messages`, data: docs });
+  });
 
 })).post(
-    body("from").trim().isMongoId(),
-    body("to").trim().isMongoId(),
-    body("subject").trim().notEmpty().isString().isLength({ max: 50 }).escape(),
-    body("text").trim().notEmpty().isString().isLength({ max: 200 }).escape(),
-    asynHandler(async (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                success: false, message: `${errors.array({ onlyFirstError: true })[0]}`,
-            });
-        }
-        next();
-    }),
-    setMessage);
+  authenticationCheck,
+  body("from").trim().isMongoId(),
+  body("to").trim().isMongoId(),
+  body("subject").trim().notEmpty().isString().isLength({ max: 50 }).escape(),
+  body("text").trim().notEmpty().isString().isLength({ max: 200 }).escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        success: false, message: `${errors.array({ onlyFirstError: true })[0]}`,
+        data: errors
+      });
+    }
+    next();
+  }),
+  setMessage);
 
-// TODO: Handle-remove delete route because the database must keep history 
-// only deactive the account when the use requests delete account
-router.route('/:id').get(getMessage).delete(deleteMessage);
+router.route('/:id')
+  .get(authenticationCheck, ownershipCheck, getMessage)
+  .delete(authenticationCheck, ownershipCheck, deleteMessage);
 
 module.exports = router;
