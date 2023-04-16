68e5eafe3905fe25a0875782ce87ac0d3235794e
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-16 14:22:09 +0100

    feat: add user whom created the document

--- app/server/models/spaceModel.js
+++ app/server/models/spaceModel.js
@@ -1,46 +1,51 @@
 const mongoose = require('mongoose');
 
 const spaceSchema = new mongoose.Schema({
-    type: {
-        type: String,
-        required: true,
-        enum: {
-            values: [
-                'stunt ',
-                'lamp-Post',
-                'bridge',
-                'guerrilla',
-                'point-of-Sale',
-                'transit',
-                'outdoor',
-                'other',
-                'poster',
-                'bus',
-                'rail',
-                'digital',
-                'wall',
-                'billboard',
-                'shelter'],
-            message: '{VALUE} not supported here'
-        }
-    },
-    otherType: {
-        type: String,
-    },
-    location: {
-        type: Object,
-    },
-    dimension: {
-        type: Object,
-        required: true
-    },
-    imagesURL: {
-        type: Array,
-    },
-    address: {
-        type: String,
-        required: true
+  type: {
+    type: String,
+    required: true,
+    enum: {
+      values: [
+        'stunt ',
+        'lamp-Post',
+        'bridge',
+        'guerrilla',
+        'point-of-Sale',
+        'transit',
+        'outdoor',
+        'other',
+        'poster',
+        'bus',
+        'rail',
+        'digital',
+        'wall',
+        'billboard',
+        'shelter'],
+      message: '{VALUE} not supported here'
     }
+  },
+  user: {
+    type: String,
+    required: true,
+    ref: 'User',
+  },
+  otherType: {
+    type: String,
+  },
+  location: {
+    type: Object,
+  },
+  dimension: {
+    type: Object,
+    required: true
+  },
+  imagesURL: {
+    type: Array,
+  },
+  address: {
+    type: String,
+    required: true
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Space', spaceSchema);
