e48c1b557195ea590583bcbed422f09d96f79818
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-16 15:28:46 +0100

    remove setter function from profile model because the URL is complete and no computation is required to determine the right image to load

--- app/server/models/profileModel.js
+++ app/server/models/profileModel.js
@@ -1,19 +1,12 @@
 const mongoose = require('mongoose');
 
-const imagesRootPath = ''; // todo - images root path
-
 const profileSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    },
-    avatar: {
-        type: String,
-
-        // append the file name to the root path
-        get: imageName => `${imagesRootPath}/${imageName}`
-    }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  avatar: String,
 }, { timestamps: true });
 
 module.exports = mongoose.model('Profile', profileSchema);
