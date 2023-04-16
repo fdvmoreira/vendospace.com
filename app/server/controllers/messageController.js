5ceee2177564bcdb3740a0e286158a4003b82073
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-16 18:42:50 +0100

    refactor: return JSON that object that has the shape consistent with other endpoints

--- app/server/controllers/messageController.js
+++ app/server/controllers/messageController.js
@@ -3,44 +3,43 @@ const Message = require('./../models/messageModel');
 
 // get Message by id
 const getMessage = asyncHandler(async (req, res) => {
-    Message.findById(req.params.id, (err, doc) => {
-        if (err) res.status(400).json({ success: false, message: `${err.message}` });
-
-        res.status(200).json(doc);
-    });
+  Message.findById(req.params.id, (err, message) => {
+    if (err) return res.status(400).json({ success: false, message: `Error: ${err.message}`, data: err });
+    if (!message) return res.status(404).json({ success: false, message: "Message not found", data: null });
+    res.json({ success: true, messages: "Message found", data: message });
+  });
 });
 
 // create Message
 const setMessage = asyncHandler(async (req, res) => {
-    const { from, to, subject, text } = req.body;
-    Message.create({ from, to, subject, text }, (err, doc) => {
-        if (err) res.status(400).json({ success: false, message: `${err.message}` });
-
-        res.status(201).json({ success: true, message: `Message ${doc.id} created` });
-    });
+  const { from, to, subject, text } = req.body;
+  Message.create({ from, to, subject, text }, (err, message) => {
+    if (err) return res.status(400).json({ success: false, message: `Error: ${err.message}`, data: err });
+    res.status(201).json({ success: true, message: `Message ${doc.id} created`, data: message });
+  });
 });
 
 //update  Message
+//TODO: remove this because we cannot update sent Messages
 const updateMessage = asyncHandler(async (req, res) => {
-    const body = req.body;
-    Message.findByIdAndUpdate(req.params.id, { body }, (err, doc) => {
-        if (err) res.status(400).json({ success: false, message: `${err.message}` });
-
-        res.status(201).json({ success: true, message: `Message ${doc._id} updated` });
-    });
+  const body = req.body;
+  Message.findByIdAndUpdate(req.params.id, { body }, (err, message) => {
+    if (err) return res.status(400).json({ success: false, message: `${err.message}`, data: err });
+    res.status(201).json({ success: true, message: `Message ${message?._id} updated`, data: message });
+  });
 });
 
 // delete Message
 const deleteMessage = asyncHandler(async (req, res) => {
-    Message.findByIdAndDelete(req.params.id, (err, doc) => {
-        if (err) res.status(400).json({ success: false, message: `${err.message}` });
-
-        res.status(200).json({ success: true, message: `Message ${doc?.id} deleted` });
-    });
+  Message.findByIdAndDelete(req.params.id, (err, message) => {
+    if (err) return res.status(400).json({ success: false, message: `Error: ${err.message}`, data: err });
+    res.status(200).json({ success: true, message: `Message ${message?._id} deleted`, data: null });
+  });
 });
+
 module.exports = {
-    getMessage,
-    setMessage,
-    updateMessage,
-    deleteMessage
+  getMessage,
+  setMessage,
+  updateMessage,
+  deleteMessage
 }
