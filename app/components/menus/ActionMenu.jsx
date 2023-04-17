9426e3c2f05115078c64dc14e046198c40764083
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-17 23:10:57 +0100

    fix the class property to className for compatibility with react

--- app/components/menus/ActionMenu.jsx
+++ app/components/menus/ActionMenu.jsx
@@ -3,14 +3,20 @@ import Link from "next/link";
 const ActionMenu = ()=> {
 
   return (
-    <div class="dropdown">
-      <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <div className="dropdown">
+      <button className="btn btn-primary dropdown-toggle" 
+        type="button" 
+        id="dropdownMenuButton" 
+        data-bs-toggle="dropdown" 
+        aria-haspopup="true" 
+        aria-expanded="false"
+        >
         create new
       </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <Link class="dropdown-item" href="/adspaces/new">Ad space</Link>
-        <Link class="dropdown-item" href="/listings/new">Listing</Link>
-        <Link class="dropdown-item" href="/auctions/new">Auction</Link>
+      <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <Link className="dropdown-item" href="/adspaces/new">Ad space</Link>
+        <Link className="dropdown-item" href="/listings/new">Listing</Link>
+        <Link className="dropdown-item" href="/auctions/new">Auction</Link>
       </div>
     </div>
   );
