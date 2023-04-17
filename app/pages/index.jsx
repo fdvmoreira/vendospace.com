4675ec32bf25bd186f334a2b58c51bd0437f6b3f
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-17 23:41:57 +0100

    delete server side request async function

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -23,7 +23,7 @@ export default (props) => {
       .then((data) => {
         setAuctions(data);
       })
-      .catch((err) => console.error(err));
+      .catch(console.error);
 
     /** Listings */
     fetch(LISTINGS_API_URL)
@@ -31,14 +31,17 @@ export default (props) => {
       .then((data) => {
         setListings(data);
       })
-      .catch((err) => console.error(err));
+      .catch(console.error);
 
     let {data, auth_success} = param.query;
+    
     //TODO: DO NOT USE this env IN PRODUCTION
-    if(data && auth_success) data = JSON.parse(CryptoJS.AES.decrypt(data, process.env.NEXT_PUBLIC_JWT_SECRET).toString(CryptoJS.enc.Utf8));
+    if(data && auth_success) {
+      data = JSON.parse(CryptoJS.AES.decrypt(data, process.env.NEXT_PUBLIC_JWT_SECRET).toString(CryptoJS.enc.Utf8));
+    }
     
     /** Authenticate the user */
-    if(auth_success&& data&&!auth.isAuthenticated) {
+    if(auth_success&& data&&!auth?.isAuthenticated) {
       updateAuth(data);
     }
       
@@ -46,44 +49,31 @@ export default (props) => {
 
   return (
     <div className='container'>
-      {
-        auth?.user?.name&&<span className="alert alert-success">Welcome {auth?.user?.name}</span>
-      }
       <hr />
-      {/**
-       * // TODO: display the listings and auctions according to the current sort order
-       */}
+      {
+        //TODO: display the listings and auctions according to the current sort order
+      }
       <ul className='list-group'>
-        {auctions.map((auction) => {
-          return (
-            <li key={auction._id} className='list-group-item list-group-flush'>
-              <AuctionCard auction={auction} />
-            </li>
-          );
-        })}
+        {
+          auctions.map((auction) => {
+            return (
+              <li key={auction?._id} className='list-group-item list-group-flush'>
+                <AuctionCard auction={auction} />
+              </li>
+            );
+          })
+        }
 
-        {listings.map((listing) => {
-          return (
-            <li key={listing._id} className='list-group-item list-group-flush'>
-              <ListingCard listing={listing} />
-            </li>
-          );
-        })}
+        {
+          listings.map((listing) => {
+            return (
+              <li key={listing?._id} className='list-group-item list-group-flush'>
+                <ListingCard listing={listing} />
+              </li>
+            );
+          })
+        }
       </ul>
     </div>
   );
 };
-
-/**
- * server-side rendering for main page content
- * @param {*} context
- * @returns
- */
-export async function getServerSideProps(context) {
-  //TODO: fetch data from server side
-  return {
-    props: {
-      data: "my testing data",
-    },
-  };
-}
