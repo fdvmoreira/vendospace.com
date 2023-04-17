614c7c785407d1403ab8a4191a31a41bdd32ef10
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-18 00:24:12 +0100

    update capitalized type and status to lowercase enum input text

--- app/server/models/accountModel.js
+++ app/server/models/accountModel.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
 
 const accountSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    },
-    type: {
-        type: String,
-        enum: {
-            values: ['Personal', 'Professional', 'Business'], // types of accounts
-            message: '{VALUES} not supported here'
-        }
-    },
-    status: {
-        type: String,
-        enum: {
-            values: ['Active', 'Disabled', 'Pending', 'Frozen', 'Deleted'], // status of account here
-            message: '{VALUE} not supported here'
-        }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  type: {
+    type: String,
+    enum: {
+      values: ['personal', 'professional', 'business'], // types of accounts
+      message: '{VALUES} not supported here'
     }
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ['active', 'disabled', 'pending', 'frozen', 'deleted'], // status of account here
+      message: '{VALUE} not supported here'
+    }
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Account', accountSchema);
