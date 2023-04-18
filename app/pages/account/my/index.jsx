2eaa73f318b9148c77cc36458173d17a84f4df54
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-18 18:49:28 +0100

    feat: add settings component and change the tabs layout to horizontal

--- app/pages/account/my/index.jsx
+++ app/pages/account/my/index.jsx
@@ -2,75 +2,80 @@ import { Suspense } from "react";
 import History from "../../../components/profile/history/History";
 import Message from "../../../components/profile/message/Message";
 import Profile from "../../../components/profile/profile/Profile";
+import Setting from "../../../components/profile/setting/Setting";
 
 export default function MyAccount(props) {
   return (
     <div className='container'>
-      <div className='d-flex align-items-start'>
-        <div
-          className='nav flex-column nav-pills me-3'
+      <div className='d-flex align-items-start flex-column'>
+        <nav
+          className='nav nav-pills me-3'
           id='v-pills-tab'
-          role='tablist'
-          aria-orientation='vertical'>
-          <button
-            className='nav-link active'
-            id='v-pills-home-tab'
-            data-bs-toggle='pill'
-            data-bs-target='#v-pills-home'
-            type='button'
-            role='tab'
-            aria-controls='v-pills-home'
-            aria-selected='true'>
-            Account history
-          </button>
-          <button
-            className='nav-link'
+          role='tablist'>
+          <a
+            className='nav-item nav-link btn active'
             id='v-pills-profile-tab'
             data-bs-toggle='pill'
             data-bs-target='#v-pills-profile'
-            type='button'
+            type='a'
             role='tab'
             aria-controls='v-pills-profile'
             aria-selected='false'>
             Profile
-          </button>
-          <button
-            className='nav-link'
+          </a>
+          <a
+            className='nav-item nav-link btn'
             id='v-pills-messages-tab'
             data-bs-toggle='pill'
             data-bs-target='#v-pills-messages'
-            type='button'
+            type='a'
             role='tab'
             aria-controls='v-pills-messages'
             aria-selected='false'>
             Messages
-          </button>
-          <button
-            className='nav-link'
+          </a>
+          <a
+            className='nav-item nav-link btn'
             id='v-pills-settings-tab'
             data-bs-toggle='pill'
             data-bs-target='#v-pills-settings'
-            type='button'
+            type='a'
             role='tab'
             aria-controls='v-pills-settings'
             aria-selected='false'>
             Settings
-          </button>
-        </div>
+          </a>
+          <a
+            className='nav-item nav-link btn'
+            id='v-pills-home-tab'
+            data-bs-toggle='pill'
+            data-bs-target='#v-pills-home'
+            type='a'
+            role='tab'
+            aria-controls='v-pills-home'
+            aria-selected='true'>
+            Account history
+          </a>
+          
+        </nav>
         <div className='tab-content' id='v-pills-tabContent'>
           <div
             className='tab-pane fade show active'
             id='v-pills-home'
             role='tabpanel'
             aria-labelledby='v-pills-home-tab'>
-            <History />
+              <Suspense fallback={<p>Loading History ...</p>}>
+                <History />
+              </Suspense>
           </div>
           <div
             className='tab-pane fade'
             id='v-pills-profile'
             role='tabpanel'
             aria-labelledby='v-pills-profile-tab'>
-            <Profile />
+              <Suspense fallback={<p>Loading Profile ...</p>}>
+                <Profile />
+              </Suspense>
           </div>
           <div
             className='tab-pane fade'
@@ -86,7 +91,9 @@ export default function MyAccount(props) {
             id='v-pills-settings'
             role='tabpanel'
             aria-labelledby='v-pills-settings-tab'>
-            Settings
+              <Suspense fallback={<p>Loading Settings ...</p>}>
+                <Setting />
+              </Suspense>
           </div>
         </div>
       </div>
