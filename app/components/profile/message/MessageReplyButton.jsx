021698f8ca2609d2e34cda2c7879a31704bc4576
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-19 00:27:28 +0100

    feat: create reply button so that user can reply to messages

---
+++ app/components/profile/message/MessageReplyButton.jsx
@@ -0,0 +1,17 @@
+import NewMessage from "../../NewMessage"
+
+const MessageReplyButton = ({to, subject})=> {
+
+  return (
+    <>
+      <button className="btn btn-sm btn-outline-primary"
+      data-bs-toggle="modal"
+      data-bs-target="#messageModal"
+      ><i className="bi bi-reply">&nbsp;Reply</i></button>
+      <NewMessage  recipient={to} subject={"RE: "+subject}/>
+    </>
+    
+  )
+}
+
+export default MessageReplyButton
