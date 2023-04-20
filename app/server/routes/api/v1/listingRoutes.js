2b560311614601133db37554a932a63488e33b39
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-20 22:59:10 +0100

    feat: protect the routes to create, get by id and delete listings

--- app/server/routes/api/v1/listingRoutes.js
+++ app/server/routes/api/v1/listingRoutes.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const asynHandler = require("express-async-handler");
-const router = express.Router();
-const { getListing, setListing, deleteListing } = require('../../../controllers/listingController');
-const Listing = require('../../../models/listingModel');
+const router = require('express').Router();
+const authenticationCheck = require("../../../middlewares/auth/authenticationCheck");
+const {
+  getAllListings,
+  getListingById,
+  setListing,
+  deleteListing
+} = require('../../../controllers/listingController');
+const ownershipCheck = require('../../../middlewares/auth/ownershipCheck');
 
-router.route("/").get(asynHandler(async (req, res) => { // todo - remove this route
-    Listing.find((err, doc) => {
-        if (err) res.status(400).json({ error: `You've messed up: ${err.message}` });
-        res.status(200).json(doc);
-    });
+router.route("/")
+  .get(getAllListings)
+  .post(authenticationCheck, setListing);
 
-})).post(setListing);
-
-// todo -remove delete route because the database must keep history 
-// only deactive the account when the use requests delete account
-router.route('/:id').get(getListing).delete(deleteListing);
+router.route('/:id')
+  .get(authenticationCheck, getListingById)
+  .delete(ownershipCheck, authenticationCheck, deleteListing);
 
 module.exports = router;
