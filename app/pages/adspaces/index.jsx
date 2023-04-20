b18eda9e67f1415c01b094efcbeae422779361ef
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-20 21:30:19 +0100

    feat: add interface to list all spaces created by the user

--- app/pages/adspaces/index.jsx
+++ app/pages/adspaces/index.jsx
@@ -1,41 +1,96 @@
-import React from "react";
-import AdSpaceCard from "../../components/adSpaceCard";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import RestrictedArea from "../../components/RestrictedArea";
+import SpaceCard from "../../components/space/SpaceCard";
+import { useAuth } from "../../context/authContext";
+import notify from "../../utils/notify";
 
-export default function AdSpace(props) {
-  const [data, data2] = props.data;
-  const cards = props.data.map((item, index) => {
-    return <AdSpaceCard data={item} />;
-  });
+const SpaceList = ()=> {
+
+  let router = useRouter();
+  let [auth,_] = useAuth();
+  let [adspaces, setAdspaces] = useState();
+
+  useEffect(()=>{
+    fetch(`/api/v1/users/${auth?.user?._id??0}/spaces`,{
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${auth?.token}`, 
+      },
+    })
+    .then(response => response.json())
+    .then(data => {
+      if(data?.success) setAdspaces(data?.data);
+    })
+    .catch(console.error);
+
+  },[]);
+
+  const editSpace = (space) =>{
+    let searchParams = new URLSearchParams(space);
+    router.push(`/adspaces/${searchParams.get('_id')}/edit`);
+  }
+
+  const deleteSpace = (id)=>{
+    const SPACE_DELETE_API = `/api/v1/users/${auth?.user?._id??0}/spaces/${id??0}`;
+
+    fetch(SPACE_DELETE_API,{
+      method: 'DELETE',
+      headers:{
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${auth?.token}`,
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      notify(data?.message, data?.success);
+      setAdspaces(adspaces.filter(space => (space?._id != id)));
+
+      if(data?.success){
+        setAdspaces(adspaces.filter(space => space?._id != id));
+      }
+    })
+    .catch(console.error);
+  }
+
+  if(!auth?.isAuthenticated) return <RestrictedArea/>
+  
+  if(!adspaces.length) return (
+  <div className="lead d-flex flex-column align-items-center gap-2">
+    <p>You haven't created any ad space yet.</p>
+    <p>
+      <Link href="/adspaces/new" className="btn btn-primary bi bi-plus-lg">&nbsp;create new ad space</Link>
+    </p>
+    <ToastContainer/>
+  </div>
+  )
 
   return (
-    <div div className='container d-flex justify-content-between mt-5'>
-      {
-        //<AdSpaceCard data={data} />
-        cards.map((item) => item)
-      }
+    <div className='container'>
+      <ul className="list-unstyled d-flex gap-2 justify-content-center">
+        {
+          adspaces?.map(space =>{
+            return (
+              <li key={space?._id}>
+                {
+                  auth?.isAuthenticated&&(
+                  <div className="d-flex bg-light justify-content-end gap-2 mb-1">
+                    <button className="bi bi-pen btn btn-sm btn" onClick={()=>editSpace(space)}>&nbsp;edit</button>
+                    <button className="btn btn-sm btn-danger" 
+                    onClick={()=>deleteSpace(space?._id)}><i className="bi bi-x-lg"></i>&nbsp;delete</button>
+                  </div>)
+                }
+                <SpaceCard data={space}/>
+              </li>
+            )
+          })
+        }
+      </ul>
+      <ToastContainer/>
     </div>
   );
 }
 
-export async function getServerSideProps(context) {
-  return {
-    props: {
-      data: [
-        {
-          type: "Roadside",
-          address: "78 Jaslow Garlic",
-          dimension: { width: 7, height: 9 },
-          location: { latitude: 78, logitude: 41 },
-          images: ["/logo.jpeg", "/logo.jpeg"],
-        },
-        {
-          type: "Billboard",
-          address: "20  Jakare Cosl",
-          dimension: { width: 20, height: 12 },
-          location: { latitude: 8, logitude: 4 },
-          images: ["/logo.jpeg", "/logo.jpeg"],
-        },
-      ],
-    },
-  };
-}
+export default SpaceList;
