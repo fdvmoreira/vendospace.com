638625af8616294739bb3691b1c0dc6c84bf0f76
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-20 21:54:16 +0100

    feat: add 2 more state a listing can be in

--- app/server/models/listingModel.js
+++ app/server/models/listingModel.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose');
 
 const listingSchema = new mongoose.Schema({
-    space: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Space'
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
-    },
-    status: {
-        type: String,
-        required: true,
-        enum: {
-            values: ['active', 'rented', 'disabled'], // todo - listing status here 
-            message: '{VALUE} not supported here'
-        }
+  space: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Space'
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: {
+      values: ['active', 'sold', 'rented', 'disabled', 'deleted'],
+      message: '{VALUE} not supported here'
     }
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Listing', listingSchema);
