82195a3f7e875961daabb80cf1189857e88d3e30
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-22 23:39:41 +0100

    feat: add page to list available spaces and select one to create auction

--- app/pages/auctions/new.jsx
+++ app/pages/auctions/new.jsx
@@ -1,171 +1,70 @@
-import { yupResolver } from "@hookform/resolvers/yup";
 import Link from "next/link";
-import { useForm } from "react-hook-form";
-import { ToastContainer } from "react-toastify";
-import * as yup from "yup";
+import { useEffect, useState } from "react";
+import RestrictedArea from "../../components/RestrictedArea";
+import AuctionForm from "../../components/auction/AuctionForm";
+import SpaceCard from "../../components/space/SpaceCard";
 import { useAuth } from "../../context/authContext";
-import notify from "../../utils/notify";
 
-const AUCTION_API_URL = "/api/v1/auctions";
-let userId = "622f55d4b3763981e2e825df";
-
-export default function Auction({ data }) {
+const AuctionCreate = ({ data })=> {
+  
   const [auth, _] = useAuth();
+  let [spaces, setSpaces] = useState();
+  let [selectedSpace, setSelectedSpace] = useState();
 
-  const schema = yup.object().shape({
-    user: yup.string().required().label("User"),
-    space: yup.string().required().label("Space"),
-    start: yup
-      .date()
-      .required()
-      .test("start-after-now", "This date has passed", (startdate, context) => {
-        return startdate.getTime() > Date.now();
-      })
-      .typeError("Start date and time are required"),
-    end: yup
-      .date()
-      .required()
-      .test(
-        "is-after-start",
-        "End data is before start date",
-        (value, context) => value.getTime() > context.parent.start.getTime(),
-      )
-      .typeError("End date and time are required"),
-    initialPrice: yup
-      .number()
-      .positive()
-      .min(0)
-      .required()
-      .typeError("Initial price required"),
-    status: yup
-      .string()
-      .oneOf(["open", "closed", "pending"])
-      .required()
-      .typeError("Select the status"),
-  });
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
+  useEffect(()=> {
+    if (auth?.isAuthenticated) loadSpaces();
+  }, []);
+
+  const loadSpaces = ()=> {
+    const SPACE_API = `/api/v1/users/${auth?.user?._id??0}/spaces`;
+    fetch(SPACE_API, {
+      headers:{
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${auth?.token??null}`,
+      }
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      if(data?.success) setSpaces(data?.data);
+    })
+    .catch(console.error);
+  };
+
+  if(!auth?.isAuthenticated) return <RestrictedArea/>
+
+  if(auth?.isAuthenticated && !spaces?.length) {
+    return (
+      <div className="lead text-center">
+        <p>You have not create any space</p>
+        <Link href='/adspaces' className="btn btn-lg btn-primary">create new ad space</Link>
+      </div>)
+  }
 
   return (
-    <div
-      className='container justify-content-center border'
-      style={{ width: "320px" }}>
-      <h1 className='lead text-center'>Create new auction</h1>
-      <hr className='hr' />
-
-      <form onSubmit={handleSubmit(submitHandler)}>
-        {/** User ID */}
-        <input type='hidden' value={auth?.user?._id} {...register("user")} />
-        {/** user error check */}
-        {errors.user?.message && (
-          <p className='text-danger'>{errors.user?.message}</p>
-        )}
-        {/** space ID */}
+    <div className="container">
+      <ul className="d-flex gap-4 flex-column align-items-center list-unstyled">
         {
-          //TODO implement space selection or creation
+          spaces?.map(space =>{
+            return (
+              <li key={space?._id} className="bg-light card">
+                <div className="card-header">
+                  <button type="button"
+                    onClick={(e)=>{setSelectedSpace(space?._id)}} 
+                    className="btn btn-primary" 
+                    data-bs-toggle="modal" 
+                    data-bs-target="#newAuctionModal">
+                  Auction this ad space
+                </button>
+                </div>
+                <SpaceCard data={space}/>
+              </li>
+            )
+          })
         }
-        <input
-          type='hidden'
-          value={"62d66ac134e94039f9bb8883"}
-          {...register("space")}
-        />
-        {/** space error check */}
-        {errors.space?.message && (
-          <p className='text-danger'>{errors.space?.message}</p>
-        )}
-        {/** start date */}
-        <label htmlFor='start' className='form-label'>
-          Start date
-        </label>
-        <input
-          className='form-control mb-2'
-          type='datetime-local'
-          id='start'
-          {...register("start")}
-        />
-        {/** start date error check */}
-        {errors.start?.message && (
-          <p className='text-danger'>{errors.start?.message}</p>
-        )}
-        {/** end date */}
-        <label htmlFor='end' className='form-label'>
-          End date
-        </label>
-        <input
-          className='form-control mb-2'
-          type='datetime-local'
-          id='end'
-          {...register("end")}
-        />
-        {/** end date error check */}
-        {errors.end?.message && (
-          <p className='text-danger'>{errors.end?.message}</p>
-        )}
-        {/** initial price */}
-        <div className='input-group flex-nowrap mb-2'>
-          <span className='input-group-text'>&pound;</span>
-          <input
-            type='number'
-            className='form-control'
-            placeholder='initial price'
-            aria-label='initial price'
-            {...register("initialPrice")}
-          />
-        </div>
-        {/** initial price error check */}
-        {errors.initialPrice?.message && (
-          <p className='text-danger'>{errors.initialPrice?.message}</p>
-        )}
-        {/** auction status */}
-        <select
-          className='form-select mb-2'
-          defaultValue={""}
-          {...register("status")}>
-          <option value=''>Auction status</option>
-          <option value='open'>Open</option>
-          <option value='closed'>Closed</option>
-          <option value='pending'>Pending</option>
-        </select>
-        {/** auction error check */}
-        {errors.status?.message && (
-          <p className='text-danger'>{errors.status?.message}</p>
-        )}
-        {/** TODO cancel button */}
-        <Link href='/' className='btn btn-outline-secondary border border-none'>
-          Cancel
-        </Link>
-        {/** submit button */}
-        <input
-          type='submit'
-          className='btn btn-success ms-2'
-          value={"Create auction"}
-        />
-        <ToastContainer />
-      </form>
+      </ul>
+      <AuctionForm space={selectedSpace}/>
     </div>
   );
 }
 
-async function submitHandler(data, event) {
-  // event.preventDefault();
-  // console.log(data);
-
-  await fetch(AUCTION_API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.success) notify("Auction Created");
-      console.log(data);
-    })
-    .catch(console.error);
-}
+export default AuctionCreate;
