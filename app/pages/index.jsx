7f6886200740a39198f0e7227ba7b305f54d7ba3
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-22 18:03:01 +0100

    feat: extract fetching and authentication into separated functions

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,14 +1,17 @@
 import CryptoJS from "crypto-js";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import AuctionCard from "../components/auctionCard";
-import ListingCard from "../components/listingCard";
+import AuctionCard from "../components/auction/AuctionCard";
+import ListingCard from "../components/listing/ListingCard";
 import { useAuth } from "../context/authContext";
 
-const AUCTIONS_API_URL = "/api/v1/auctions";
-const LISTINGS_API_URL = "/api/v1/listings";
+const AUCTIONS_PUBLIC_API = "/api/v1/auctions/public/1";
+const AUCTIONS_AUTH_API = "/api/v1/auctions";
+const LISTINGS_PUBLIC_API = "/api/v1/listings/public/1";
+const LISTINGS_AUTH_API = "/api/v1/listings";
 
 export default (props) => {
+  
   let [auctions, setAuctions] = useState([]);
   let [listings, setListings] = useState([]);
 
@@ -16,45 +19,61 @@ export default (props) => {
   let [auth, updateAuth] = useAuth();
 
   useEffect(() => {
+    authenticate();
+    
+    /** Auctions */
+    getAuctions((auth?.isAuthenticated)?AUCTIONS_AUTH_API:AUCTIONS_PUBLIC_API);
+
+    /** Listings */
+    getListings((auth?.isAuthenticated)?LISTINGS_AUTH_API:LISTINGS_PUBLIC_API);
+  }, []);
+
+  const getAuctions = (api) =>{
+    fetch(api,{
+      headers:{
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${auth?.token}}`,
+      }
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      if(data?.success) setAuctions(()=>data?.data); 
+    })
+    .catch(console.error);
+  }
+
+  const getListings = (api) =>{
+    fetch(api,{
+      headers:{
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${auth?.token}}`,
+      }
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      if(data?.success) setListings(()=>data?.data); 
+    })
+    .catch(console.error);
+  }
+
+  const authenticate =() =>{
     const urlSearchParams = new URLSearchParams(params.asPath);
     let auth_success = urlSearchParams.get("/?auth_success"); //TODO: parameter name corrupted. Check the origin  
     let data = urlSearchParams.get("data");
 
-    //TODO: DO NOT USE ''NEXT_PUBLIC_JWT_SECRET'' env IN PRODUCTION
+    //TODO: DO NOT USE "NEXT_PUBLIC_JWT_SECRET" env IN PRODUCTION
     if(!auth?.isAuthenticated && data && auth_success) {
       data = JSON.parse(CryptoJS.AES.decrypt(data, process.env.NEXT_PUBLIC_JWT_SECRET).toString(CryptoJS.enc.Utf8));
       updateAuth(data);
     }
-
-    /** Auctions */
-    fetch(AUCTIONS_API_URL)
-      .then((res) => res.json())
-      .then((data) => {
-        setAuctions(data);
-      })
-      .catch(console.error);
-
-    /** Listings */
-    fetch(LISTINGS_API_URL)
-      .then((res) => res.json())
-      .then((data) => {
-        setListings(data);
-      })
-      .catch(console.error);
-    
-    
-      
-  }, []);
+  }
 
   return (
     <div className='container'>
       <hr />
-      {
-        //TODO: display the listings and auctions according to the selected sorting order
-      }
       <ul className='list-group'>
         {
-          auctions.map((auction) => {
+          auctions?.map((auction) => {
             return (
               <li key={auction?._id} className='list-group-item list-group-flush'>
                 <AuctionCard auction={auction} />
@@ -64,7 +83,7 @@ export default (props) => {
         }
 
         {
-          listings.map((listing) => {
+          listings?.map((listing) => {
             return (
               <li key={listing?._id} className='list-group-item list-group-flush'>
                 <ListingCard listing={listing} />
@@ -75,4 +94,4 @@ export default (props) => {
       </ul>
     </div>
   );
-};
+}
