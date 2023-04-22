b4410e4aa37415decc6c4cb3253c8e047403213e
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-22 17:53:16 +0100

    refactor the Listing card component

--- app/components/listing/ListingCard.jsx
+++ app/components/listing/ListingCard.jsx
@@ -1,64 +1,82 @@
 import Image from "next/legacy/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
+import { useAuth } from "../../context/authContext";
 import NewMessage from "../NewMessage";
 
-export default function ListingCard({ listing }) {
+const ListingCard = ({ listing }) => {
   const { _id, space: spaceId, createdAt, user, status } = listing;
   let [space, setSpace] = useState();
+
+  let [auth,_] = useAuth();
   
+  const SPACE_PUBLIC_API = `/api/v1/spaces/${spaceId}/public/1`;
+  const SPACE_AUTH_API = `/api/v1/spaces/${spaceId}`;
+
   useEffect(() => {
-    const SPACE_API_URL = `/api/v1/spaces/${spaceId}/public`;
-    fetch(SPACE_API_URL)
-      .then((res) => res.json())
-      .then((data) => {
-        setSpace(()=>data?.data);
-      })
-      .catch(console.error);
+    getSpace();
   }, []);
+
+  const getSpace = () =>{
+    fetch((auth?.isAuthenticated)? SPACE_AUTH_API:SPACE_PUBLIC_API,{
+      headers:{
+        'Accept':'application/json',
+        'Authorization': `Bearer ${auth?.token}`,
+      }
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      if(data?.success) setSpace(data?.data);
+    })
+    .catch(console.error);
+  }
+
   return (
     <div className='card'>
-      <div className='card-header'>
-        <p>
-          <small className='text text-uppercase'>
-            {String(user).substring(0, 6)}
-          </small>
-        </p>
-        <Image
-          src={space?.imagesURL?.[0] || "/logo.jpeg"}
-          alt={`${space?.type} ${space?.dimension.width}x${space?.dimension.height} ${space?.dimension.unit}`}
-          height={200}
-          width={200}
-          className='card-img-top'
-        />
+      <div className='card-header d-flex justify-content-between'>
+        <small className="text text-uppercase text-nowrap text-muted">ID:&nbsp;{_id}</small>
+        <small className='text-muted'>Posted&nbsp;{new Date(createdAt).toLocaleDateString()}</small>
       </div>
       <div className='card-body'>
-        <h6>
-          <small className='card-text'>
-            {status.toLocaleUpperCase()}&nbsp;
-          </small>
-          <small className='text-muted'>
-            {new Date(createdAt).toLocaleDateString()}
-          </small>
-        </h6>
-        <h5 className='card-title text-uppercase'>
-          {`${space?.type} ${space?.dimension.width}x${space?.dimension.height} ${space?.dimension.unit}`}
-        </h5>
-        <p className='card-text'>{space?.address}</p>
-        <p className='card-text'>
-          <span>lat@{space?.location.latitude}</span>&#9900;
-          <span>lng@{space?.location.longitude}</span>
-        </p>
+        <div className="d-flex gap-2">
+          <Image
+            src={space?.imagesURL?.[0] || "/logo.jpeg"}
+            alt={`${space?.type} ${space?.dimension.width}x${space?.dimension.height} ${space?.dimension.unit}`}
+            height={200}
+            width={200}
+            className='card-img-top'
+          />
+          <div className="fw-light">
+            <h5 className='card-title text-uppercase'>{space?.type}</h5>
+            <p className='bi bi-mailbox'>&nbsp;{space?.address}</p>
+            <p className="bi bi-aspect-ratio d-flex justify-content-between">Dimensions:&nbsp;
+              <span>                
+                <span>{space?.dimension.width}</span>
+                &times;
+                <span>{space?.dimension.height}</span>
+              </span>
+              <span className="text-uppercase">{space?.dimension.unit}</span>
+            </p>
+            <p className='bi bi-geo-alt'>
+              <span>&nbsp;GPS coords(DD):</span>
+              <span className="text-right"> {space?.location.latitude},{space?.location.longitude}</span>
+            </p>          
+          </div>                    
+        </div>
       </div>
       <div className='card-footer'>
-        <button
+        {(!auth?.isAuthenticated)? <button
           type='button'
           className='btn btn-primary'
           data-bs-toggle='modal'
           data-bs-target='#messageModal'>
           Message owner
         </button>
-        <NewMessage recipient={user} subject={"default subject!"} />
+        :<Link href={'/login'} className="btn btn-outline-primary btn-sm">Authenticate to message the owner</Link>}
+        <NewMessage recipient={user} subject={"Enquire about your LISTING!"} />
       </div>
     </div>
   );
 }
+
+export default ListingCard;
