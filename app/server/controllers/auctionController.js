967179b19abc3e912b4dad9907268b8c3d9f2bfe
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-22 04:45:16 +0100

    feat: add the routes and controller for auctions resources

--- app/server/controllers/auctionController.js
+++ app/server/controllers/auctionController.js
@@ -1,46 +1,52 @@
 const asyncHandler = require('express-async-handler');
 const Auction = require('./../models/auctionModel');
 
-// get Auction by id
-const getAuction = asyncHandler(async (req, res) => {
-    Auction.findById(req.params.id, (err, doc) => {
-        if (err) return res.status(400).json({ error: `${err.message}` });
-
-        res.status(200).json(doc);
-    });
+const getAllActivePublicAuctions = asyncHandler(async (req, res) => {
+  Auction.find({ status: "open" }, { user: 0 }, (error, auction) => {
+    if (error) return res.status(500).json({ success: false, message: `Error: ${error.message}`, data: error });
+    if (!auction) return res.status(404).json({ success: false, message: 'Auction not found', data: null });
+    res.json({ success: true, message: `Auction found`, data: auction });
+  });
 });
 
-// create Auction
+const getAuctionById = asyncHandler(async (req, res) => {
+  Auction.findById(req.params.id, (error, auction) => {
+    if (error) return res.status(500).json({ success: false, message: `Error: ${error.message}`, data: error });
+    if (!auction) return res.status(404).json({ success: false, message: 'Auction not found', data: null });
+    res.json({ success: true, message: `Auction found`, data: auction });
+  });
+});
+
 const setAuction = asyncHandler(async (req, res) => {
-    const { start, end, user, space, initialPrice, status } = req.body;
-    Auction.create({ start, end, user, space, initialPrice, status }, (err, doc) => {
-        if (err) return res.status(400).json({ success: false, error: `${err.message}` });
-
-        res.status(201).json({ success: true, message: `Auction ${doc.id} created` });
-    });
+  const { start, end, user, space, initialPrice, status } = req.body;
+  Auction.create({ start, end, user, space, initialPrice, status }, (error, auction) => {
+    if (error) return res.status(500).json({ success: false, message: `Error: ${error.message}`, data: error });
+    if (!auction) return res.status(404).json({ success: false, message: 'Auction not created', data: null });
+    res.status(201).json({ success: true, message: `Auction created`, data: auction });
+  });
 });
 
-//update  Auction
-const updateAuction = asyncHandler(async (req, res) => {
-    const body = req.body;
-    Auction.findByIdAndUpdate(req.params.id, { body }, (err, doc) => {
-        if (err) return res.status(400).json({ success: false, error: `${err.message}` });
-
-        res.status(201).json({ success: true, message: `Auction ${doc._id} updated` });
-    });
+const updateAuctionById = asyncHandler(async (req, res) => {
+  const body = req.body;
+  Auction.findByIdAndUpdate(req.params.id, { ...body }, { new: true }, (error, auction) => {
+    if (error) return res.status(500).json({ success: false, message: `Error: ${error.message}`, data: error });
+    if (!auction) return res.status(404).json({ success: false, message: 'Auction not found', data: null });
+    res.json({ success: true, message: `Auction  updated`, data: auction });
+  });
 });
 
-// delete Auction
-const deleteAuction = asyncHandler(async (req, res) => {
-    Auction.findByIdAndDelete(req.params.id, (err, doc) => {
-        if (err) return res.status(400).json({ success: false, error: `${err.message}` });
-
-        res.status().json({ success: true, message: `Auction ${doc?.id} deleted` });
-    });
+const deleteAuctionById = asyncHandler(async (req, res) => {
+  Auction.findByIdAndDelete(req.params.id, (error, auction) => {
+    if (error) return res.status(500).json({ success: false, message: `Error: ${error.message}`, data: error });
+    if (!auction) return res.status(404).json({ success: false, message: 'Auction not deleted', data: null });
+    res.json({ success: true, message: 'Auction deleted', data: auction });
+  });
 });
+
 module.exports = {
-    getAuction,
-    setAuction,
-    updateAuction,
-    deleteAuction
+  getAllActivePublicAuctions,
+  getAuctionById,
+  setAuction,
+  updateAuctionById,
+  deleteAuctionById
 }
--- app/server/routes/api/v1/auctionRoutes.js
+++ app/server/routes/api/v1/auctionRoutes.js
@@ -1,19 +1,31 @@
 const express = require('express');
-const asynHandler = require("express-async-handler");
+const asyncHandler = require("express-async-handler");
 const router = express.Router();
-const { getAuction, setAuction, deleteAuction } = require('../../../controllers/auctionController');
+const {
+  getAllActivePublicAuctions,
+  getAuctionById,
+  setAuction,
+  updateAuctionById,
+  deleteAuctionById,
+} = require('../../../controllers/auctionController');
 const Auction = require('../../../models/auctionModel');
+const authenticationCheck = require('../../../middlewares/auth/authenticationCheck');
+const ownershipCheck = require('../../../middlewares/auth/ownershipCheck');
 
-router.route("/").get(asynHandler(async (req, res) => { // todo - remove this route
-    Auction.find((err, doc) => {
-        if (err) res.status(400).json({ error: `You've messed up: ${err.message}` });
-        res.status(200).json(doc);
-    });
+router.route("/").get(asyncHandler(async (req, res) => { // todo - remove this route
+  Auction.find((err, doc) => {
+    if (err) res.status(400).json({ error: `You've messed up: ${err.message}` });
+    res.status(200).json(doc);
+  });
 
-})).post(setAuction);
+})).post(authenticationCheck, setAuction);
 
-// todo -remove delete route because the database must keep history 
-// only deactive the account when the use requests delete account
-router.route('/:id').get(getAuction).delete(deleteAuction);
+router.route('/:id')
+  .get(authenticationCheck, getAuctionById)
+  .patch(authenticationCheck, ownershipCheck, updateAuctionById)
+  .delete(authenticationCheck, ownershipCheck, deleteAuctionById);
+
+router
+  .get('/public/:no', getAllActivePublicAuctions);
 
 module.exports = router;
