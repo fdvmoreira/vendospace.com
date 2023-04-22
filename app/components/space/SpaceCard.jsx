95fd92bef38cca2beeefae9b135d678ef36e3115
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-22 23:17:24 +0100

    refactor: extract properties from objects when they are used instead of destructuring the individual variables

--- app/components/space/SpaceCard.jsx
+++ app/components/space/SpaceCard.jsx
@@ -2,26 +2,13 @@ import Image from "next/legacy/image";
 
 const SpaceCard = ({ data }) =>{
 
-  const {
-    type,
-    location:{latitude:latitude},
-    location:{longitude:longitude},
-    dimension:{width:width}, 
-    dimension:{height:height},
-    dimension:{unit:unit},
-    address,
-    createdAt,
-    imagesURL:images,
-  } = data;
-
-
   return (
     <div className='bg-light d-flex gap-2 flex-row'>
       <div className='card-header'>
         <div className='card-img-top'>
           <Image
-            src={images?.[0]??"/face.jpeg"}
-            alt={`${type}-${address}`}
+            src={data?.imagesURL?.[0]??"/face.jpeg"}
+            alt={`${data?.type}-${data?.address}`}
             width={200}
             height={200}
           />
@@ -32,32 +19,32 @@ const SpaceCard = ({ data }) =>{
           <ul className=" list-unstyled">
             <li>
               <ul className='d-flex list-unstyled'>
-                <li className='lead fw-bolder text-uppercase'>{type}</li>
+                <li className='lead fw-bolder text-uppercase'>{data?.type}</li>
               </ul>
             </li>
-            <li className="fw-light fs-5">{address}</li>
+            <li className="fw-light fs-5">{data?.address}</li>
             <li className="d-flex justify-content-between">
               <span>Dimensions:&nbsp;</span>
               <ul className='d-flex list-unstyled justify-content-around fw-light'>
-                <li>{width}</li>
+                <li>{data?.dimension?.width}</li>
                 <li className="bi bi-x"></li>
-                <li>{height}</li>
-                <li className="text-uppercase fw-light">&nbsp;{unit}</li>
+                <li>{data?.dimension?.height}</li>
+                <li className="text-uppercase fw-light">&nbsp;{data?.dimension?.unit}</li>
               </ul>
             </li>
             <li className="d-flex justify-content-between">
               <p className="text-nowrap">GPS Coords (DD):&nbsp;</p>
               <ul className='d-flex list-unstyled justify-content-end fw-light'>
-                <li>{latitude}</li>
+                <li>{data?.location?.latitude}</li>
                 <li>&nbsp;&bull;&nbsp;</li>
-                <li>{longitude}</li>
+                <li>{data?.location?.longitude}</li>
               </ul>
             </li>
           </ul>
         </div>
         <div className='card-footer d-flex justify-content-between bg-light'>
           <span className="text-muted">Last update</span>
-          <span className="text-muted fw-light">{new Date(createdAt).toLocaleDateString('en-GB')}</span>
+          <span className="text-muted fw-light">{new Date(data?.createdAt).toLocaleDateString('en-GB')}</span>
         </div>
       </div>
     </div>
