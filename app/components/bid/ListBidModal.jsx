4de4870c90ffde913031751fb00491e6313623a6
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-23 18:24:47 +0100

    rename property class to className

--- app/components/bid/ListBidModal.jsx
+++ app/components/bid/ListBidModal.jsx
@@ -1,16 +1,16 @@
 const ListBidsModal = ({bids}) =>{
 
   return (
-    <div class="modal fade" id="listBidsModal" tabIndex="-1" role="dialog" aria-labelledby="BidsListModal" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+    <div className="modal fade" id="listBidsModal" tabIndex="-1" role="dialog" aria-labelledby="BidsListModal" aria-hidden="true">
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div class="modal-body">
+          <div className="modal-body">
             <ul>
               {
                 bids?.map(bid =>{
@@ -27,8 +27,8 @@ const ListBidsModal = ({bids}) =>{
               }
             </ul>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
           </div>
         </div>
       </div>
