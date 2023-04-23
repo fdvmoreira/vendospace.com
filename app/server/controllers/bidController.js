44c1ed0cf09ab4468f4fade3e000def4f684628c
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-23 17:53:56 +0100

    feat: add bid routes and update the existing ones

--- app/server/controllers/bidController.js
+++ app/server/controllers/bidController.js
@@ -1,50 +1,77 @@
-const asynHandler = require('express-async-handler');
+const asyncHandler = require('express-async-handler');
 const Bid = require('./../models/bidModel');
 
-// get bid - 
-// Which parameters will be used to get the bids info ?
-const getBid = asynHandler(async (req, res) => {
-    Bid.findById(req.params.id, (err, doc) => {
-
-        if (err) return res.status(404).json({ success: false, message: `${err.message}` });
-
-        res.status(200).json(doc);
-    });
+const getBid = asyncHandler(async (req, res) => {
+  Bid.findById(req.params.id, (error, bid) => {
+    if (error) return res.status(500).json({ success: false, message: `Error: ${err.message}`, data: error });
+    if (!bid) return res.status(404).json({ success: false, message: 'Bid not found', data: null });
+    res.status(200).json({ success: true, message: `found bids`, data: bid });
+  });
 });
 
-// create bid
-const setBid = asynHandler(async (req, res) => {
-    const { bidder, auction, price } = req.body;
-    Bid.create({
-        bidder,
-        auction,
-        price
-    }, (error, doc) => {
-
-        if (error) return res.json({ success: false, message: `Error ${error.message}` });
-
-        res.status(201).json({ success: true, message: `${doc.id} created` });
-    });
+const getBidsByAuctionId = asyncHandler(async (req, res) => {
+  Bid.find({ auction: req?.params?.auctionId }, (error, bids) => {
+    if (error) return res.status(500).json({ success: false, message: `Error: ${err.message}`, data: error });
+    if (!bids) return res.status(404).json({ success: false, message: 'bids not found', data: null });
+    res.status(200).json({ success: true, message: `found bids`, data: bids });
+  });
 });
 
-// update bid
-const updateBid = asynHandler(async (req, res) => {
-    const body = req.body;
-    Auction.findByIdAndUpdate(req.params.id, { body }, (err, doc) => {
-        if (err) return res.status(400).json({ success: false, Error: `${err.message}` });
-
-        res.status(201).json({ success: true, message: `Bid ${doc._id} updated` });
-    });
+const getBidsByAuctionIdPublic = asyncHandler(async (req, res) => {
+  Bid.find({ auction: req?.params?.auctionId }, { bidder: 0 }, (error, bids) => {
+    if (error) return res.status(500).json({ success: false, message: `Error: ${err.message}`, data: error });
+    if (!bids) return res.status(404).json({ success: false, message: 'bids not found', data: null });
+    res.status(200).json({ success: true, message: `found bids`, data: bids });
+  });
 });
 
-// delete bid
-const deleteBid = asynHandler(async (req, res) => {
-    Bid.findByIdAndDelete(req.body.id, (error, doc) => {
+const getStatsByAuctionId = asyncHandler(async (req, res) => {
+  try {
+    let bidsCount = await Bid.countDocuments({ "auction": req?.params?.auctionId }).exec();
+    let highestBid = await Bid.find({ "auction": req?.params?.auctionId }).select("price").sort({ price: -1 }).limit(1).exec();
 
-        if (error) return res.status(400).json({ success: false, message: `Error: ${error.message}` });
-
-        res.status(200).json({ success: true, message: `${doc.id} deleted successfuly` });
+    res.json({
+      success: true, message: 'found stats', data: {
+        bidsCount, highestBid: highestBid?.[0]?.price ?? 0
+      }
     });
+  } catch (error) {
+    res.status(500).json({ success: false, message: `Error: ${error?.message}`, data: error });
+  }
 });
 
-module.exports = { getBid, setBid, updateBid, deleteBid }
+const setBid = asyncHandler(async (req, res) => {
+  const body = req?.body;
+  Bid.create({ ...body }, (error, bid) => {
+    if (error) return res.json({ success: false, message: `Error: ${error.message}`, data: error });
+    if (!bid) return res.status(404).json({ success: false, message: 'Bid not created', data: null });
+    res.status(201).json({ success: true, message: `bid created`, data: bid });
+  });
+});
+
+const updateBidById = asyncHandler(async (req, res) => {
+  const body = req?.body;
+  Auction.findByIdAndUpdate(req.params.id, { body }, (error, bid) => {
+    if (error) return res.status(400).json({ success: false, Error: `${error.message}`, data: error });
+    if (!bid) return res.status(404).json({ success: false, message: 'Bid not found', data: null });
+    res.status(201).json({ success: true, message: `Bid ${bid._id} updated`, data: bid });
+  });
+});
+
+const deleteBidById = asyncHandler(async (req, res) => {
+  Bid.findByIdAndDelete(req.body.id, (error, bid) => {
+    if (error) return res.status(400).json({ success: false, message: `Error: ${error.message}` });
+    if (!bid) return res.status(404).json({ success: false, message: 'Bid not found', data: null });
+    res.status(200).json({ success: true, message: `${bid.id} deleted successfully` });
+  });
+});
+
+module.exports = {
+  getBid,
+  getStatsByAuctionId,
+  getBidsByAuctionId,
+  getBidsByAuctionIdPublic,
+  setBid,
+  updateBidById,
+  deleteBidById
+}
--- app/server/routes/api/v1/bidRoutes.js
+++ app/server/routes/api/v1/bidRoutes.js
@@ -1,20 +1,34 @@
 const router = require('express').Router();
 const { body, validationResult } = require('express-validator');
-const { getBid, setBid, deleteBid } = require('../../../controllers/bidController');
+const authenticationCheck = require('../../../middlewares/auth/authenticationCheck');
+const ownershipCheck = require('../../../middlewares/auth/ownershipCheck');
+const {
+  setBid,
+  getBid,
+  getStatsByAuctionId,
+  getBidsByAuctionId,
+  getBidsByAuctionIdPublic,
+  deleteBidById,
+} = require('../../../controllers/bidController');
 
 // TODO: make sure that the bid is bigger than the current biggest bid before creating a new one
 // TODO: make sure that bidder and the auction ID exist before creating a new one
 router.post('/',
-    body("bidder").trim().escape().isMongoId(),
-    body("auction").trim().escape().isMongoId(),
-    body("price").trim().escape().isNumeric().isDecimal(),
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (errors.length > 0) { return res.status(400).json({ success: false, message: errors[0].msg }); }
-        next();
-    },
-    setBid);
+  body("bidder").trim().escape().isMongoId(),
+  body("auction").trim().escape().isMongoId(),
+  body("price").trim().escape().isNumeric().isDecimal(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.length > 0) { return res.status(400).json({ success: false, message: errors[0].msg }); }
+    next();
+  },
+  setBid);
 
-router.route('/:id').get(getBid).delete(deleteBid);
+router
+  .get('/:id', authenticationCheck, getBid)
+  .delete('/:id', authenticationCheck, ownershipCheck, deleteBidById)
+  .get('/stats/:auctionId', getStatsByAuctionId)
+  .get('/auctions/:auctionId', getBidsByAuctionId)
+  .get('/auctions/:auctionId/public/:no', getBidsByAuctionIdPublic);
 
 module.exports = router;
