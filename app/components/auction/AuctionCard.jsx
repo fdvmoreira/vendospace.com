c568ed9829db92ce59cc51b5c87160ab532ce235
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-04-23 19:38:56 +0100

    refactor the auctions card to display bids, highest bid initial price etc

--- app/components/auction/AuctionCard.jsx
+++ app/components/auction/AuctionCard.jsx
@@ -1,168 +1,140 @@
 import Image from "next/legacy/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useAuth } from "../../context/authContext";
 import NewBid from "../NewBid";
 
-export default function AuctionCard({ auction }) {
-  const {
-    _id: auctionId,
-    start,
-    end,
-    initialPrice,
-    space: spaceId,
-    createdAt,
-    user,
-    status,
-  } = auction;
-
+const AuctionCard = ({ auction }) => {
   const [auth, _] = useAuth();
   let [space, setSpace] = useState();
-  let [highestBid, setHighestBid] = useState(0);
-  let [remainingTime, setRemainingTime] = useState(
-    (Number(new Date(end).getTime()) - Number(Date.now())) / (1000 * 60 * 60),
-  );
+  let [bidStat, setBidStat] = useState(0);
 
-  const SPACE_PUBLIC_API = `/api/v1/spaces/${spaceId}/public/1`;
-  const SPACE_AUTH_API = `/api/v1/spaces/${spaceId}`;
-  const BID_PUBLIC_API = `/api/v1/bids/${spaceId}/public/1`;
+  const SPACE_PUBLIC_API = `/api/v1/spaces/${auction?.space}/public/1`;
+  const SPACE_AUTH_API = `/api/v1/spaces/${auction?.space}`;
+  const BID_STATS_API = `/api/v1/bids/stats/${auction?._id}`;
 
   useEffect(() => {
-    // fetch space data
-    fetch((auth?.isAuthenticated)? SPACE_AUTH_API:SPACE_PUBLIC_API,{
-      headers:{
-        'Accept':'application/json',
-        'Authorization': `Bearer ${auth?.token}`,
-      }
+    getSpace(auth?.isAuthenticated ? SPACE_AUTH_API : SPACE_PUBLIC_API);
+    getBidStats(BID_STATS_API);
+  }, []);
+
+  const getSpace = (api) => {
+    fetch(api, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${auth?.token ?? null}`,
+      },
     })
       .then((res) => res.json())
       .then((data) => {
-        setSpace(data.data);
+        if (data?.success) setSpace(data?.data);
       })
       .catch((err) => console.error(err));
+  };
 
-    // set remaining time interval
-    setInterval(
-      (end) => {
-        let currenTime = Number(Date.now());
-        let days = Math.floor(getTimeLeftDays(end - currenTime));
-        let hours = Math.floor(getTimeLeftHours(end - currenTime));
-        let minutes = Math.floor(getTimeLeftMinutes(end - currenTime));
-        let seconds = Math.floor(getTimeLeftSeconds(end - currenTime));
-        // console.log(Math.abs(end - Date.now()));
-        setRemainingTime(days || hours || minutes || seconds);
+  const getBidStats = (api) => {
+    fetch(api, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${auth?.token}`,
       },
-      1000,
-      Number(new Date(end).getTime()),
-    );
-  }, []);
-
-  // const getHighestBid = () =>{
-  //   const BID_API = `/api/v1/bids/${auction?._id}`;
-  // }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.success) setBidStat(data?.data);
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
-    <div className='card'>
-      <div className='card-header'>
-        {/** owner */}
-        <p>
-          <small className='text-muted text-uppercase'>
-            {String(user).substring(0, 6)}
-          </small>
-        </p>
-
-        {/** end date */}
-        <p>
-          Remaining time{" "}
-          <span className='fw-bolder text-danger'>
-            {remainingTime > 24 ? remainingTime / 24 : remainingTime}
-          </span>{" "}
-          hours
-        </p>
-        {/** highest bid */}
-        <p className='card-text'>
-          Highest bid: <span className='fw-bolder'>{highestBid}</span>
-        </p>
-        {/** image */}
-        <Image
-          src={space?.imagesURL?.[0] || "/logo.jpeg"}
-          alt={`${space?.type} ${space?.dimension.width}x${space?.dimension.height} ${space?.dimension.unit}`}
-          height={200}
-          width={200}
-          className='card-img-top'
-        />
+    <div className='card fw-light'>
+      <div className='card-header d-flex flex-column'>
+        <div className='d-flex justify-content-between'>
+          <small className='text-uppercase'>{auction?.status}&nbsp;</small>
+          <h6>
+            <small className='text-muted'>
+              Posted&nbsp;{new Date(auction?.start).toLocaleDateString()}
+            </small>
+          </h6>
+        </div>
+        <div>
+          <small className='text-muted text-uppercase'> {auction?.user} </small>
+        </div>
+        <div className='d-flex justify-content-between text-uppercase'>
+          <div className='d-flex flex-column'>
+            <span>Ends&nbsp;</span>
+            <div>
+              <span>{new Date(auction?.end).toLocaleDateString()}</span>
+              &nbsp;
+              <span className='fw-bolder text-danger lead'>
+                {new Date(auction?.end).toLocaleTimeString()}
+              </span>
+            </div>
+          </div>
+          <div className='d-flex flex-column'>
+            <span>Initial</span>
+            <h5 className='fw-bolder text-muted'>
+              {auction?.initialPrice ?? 0}&pound;
+            </h5>
+          </div>
+          <div className='d-flex flex-column'>
+            <span>Highest &pound;</span>
+            <h3 className='fw-bolder text-success'>
+              {bidStat?.highestBid ?? 0}&pound;
+            </h3>
+          </div>
+          <div className='d-flex flex-column'>
+            <span>Bids</span>
+            <h5 className='fw-bolder text-secondary'>
+              {bidStat?.bidsCount ?? 0}
+            </h5>
+          </div>
+        </div>
       </div>
-      <div className='card-body'>
-        {/** started at and status */}
-        <h6>
-          <small className='card-text text-uppercase'>{status}&nbsp;</small>
-          <small className='text-muted'>
-            {new Date(start).toLocaleDateString()}
-          </small>
-        </h6>
-
-        {/** title */}
-        <h5 className='card-title text-uppercase'>
-          {`${space?.type} ${space?.dimension.width}x${space?.dimension.height} ${space?.dimension.unit}`}
-        </h5>
-
-        {/** address */}
-        <p className='card-text'>{space?.address}</p>
-
-        {/** lat and lng coords */}
-        <p className='card-text'>
-          <span>lat@{space?.location.latitude}</span>&#9900;
-          <span>lng@{space?.location.longitude}</span>
-        </p>
+      <div className='card-body d-flex gap-3'>
+        <div>
+          <Image
+            src={space?.imagesURL?.[0] ?? "/logo.jpeg"}
+            alt={`${space?.type} ${space?.dimension?.width}x${space?.dimension?.height} ${space?.dimension?.unit}`}
+            height={200}
+            width={200}
+            className='card-img-top'
+          />
+        </div>
+        <div>
+          <p className='lead fw-bold text-uppercase'>{space?.type}</p>
+          <div className='d-flex justify-content-between'>
+            <p>Dimensions&nbsp;</p>
+            <span>{`${space?.dimension?.width}x${space?.dimension?.height} ${space?.dimension?.unit}`}</span>
+          </div>
+          <p>Address&nbsp;{space?.address}</p>
+          <div className='d-flex justify-content-between'>
+            <span>GPS Coords (DD)&nbsp;</span>
+            <div className='fw-light'>
+              <span>{space?.location?.latitude}</span>,
+              <span>{space?.location?.longitude}</span>
+            </div>
+          </div>
+        </div>
       </div>
       <div className='card-footer'>
-        <button
-          type='button'
-          className='btn btn-primary'
-          data-bs-toggle='modal'
-          data-bs-target='#bidModal'>
-          New bid
-        </button>
-        <NewBid data={{ bidderId: auth?.user?._id, auctionId: auction._id }} />
+        {auth?.isAuthenticated ? (
+          <button
+            type='button'
+            className='btn btn-primary'
+            data-bs-toggle='modal'
+            data-bs-target='#bidModal'>
+            New bid
+          </button>
+        ) : (
+          <Link href={"/login"} className='btn btn-sm btn-outline-primary'>
+            Authenticate to place a bid
+          </Link>
+        )}
+        <NewBid data={{ bidderId: auth?.user?._id, auctionId: auction?._id }} />
       </div>
     </div>
   );
-}
-/**
- * gets remaining time in days
- * @param {milliseconds} milliseconds
- * @returns
- */
-const getTimeLeftDays = (milliseconds) =>
-  Math.abs(Number(milliseconds) / (24 * 60 * 60 * 1000));
-
-/**
- * gets the remaining time in hours
- * @param {milliseconds} milliseconds
- * @returns hours
- */
-const getTimeLeftHours = (milliseconds) =>
-  Math.abs(Number(milliseconds) / (60 * 60 * 1000));
-
-/**
- * gets the remaining time in minutes
- * @param {milliseconds} milliseconds
- * @returns hours
- */
-const getTimeLeftMinutes = (milliseconds) =>
-  Math.abs(Number(milliseconds) / (60 * 1000));
-
-/**
- * gets remaining time in seconds
- * @param {milliseconds} milliseconds
- * @returns
- */
-const getTimeLeftSeconds = (milliseconds) =>
-  Math.abs(Number(milliseconds) / 1000);
-
-export {
-  getTimeLeftDays,
-  getTimeLeftHours,
-  getTimeLeftMinutes,
-  getTimeLeftSeconds
 };
 
+export default AuctionCard;
