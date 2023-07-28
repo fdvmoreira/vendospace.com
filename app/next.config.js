af946d1cee0523691b043671eca8a961c006f177
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-07-28 21:27:35 +0100

    Fixed #70 by deleting the exposed data and update the dot ignore file

--- app/next.config.js
+++ app/next.config.js
@@ -7,16 +7,6 @@ const nextConfig = {
       "media.licdn.com"
     ],
   },
-  env: {
-    HOSTNAME: "localhost",
-    PORT: 3000,
-
-    CLOUDINARY_API_KEY: "256417812456237",
-    CLOUDINARY_API_SECRET: "U9WovbWDP3koenUMQb2_B46g_Zk",
-    CLOUDINARY_CLOUD_NAME: "vendospace-com",
-    CLOUDINARY_UNAUTH_UPLOAD_URL: `https://api.cloudinary.com/v1_1/vendospace-com/auto/upload`,
-    CLOUDINARY_UNAUTH_UPLOAD_PRESET: "aici91ve",
-  },
   reactStrictMode: true,
 }
 
