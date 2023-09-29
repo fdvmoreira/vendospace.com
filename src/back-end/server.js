a4eaf3864c57318029e4ea56350baf4fa1327143
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-09-29 18:13:02 +0100

    remove redundant code

--- src/back-end/server.js
+++ src/back-end/server.js
@@ -1,15 +1,13 @@
 const express = require("express");
-const next = require('next');
-const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
+const next = require("next");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 require("colors");
 require("dotenv").config();
 
-
 const PORT = process.env.PORT || 3000;
-const path = require("path");
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 
 // next app
 const nextApp = next({ dev });
@@ -17,15 +15,14 @@ const nextApp = next({ dev });
 // request handler for nextjs routes
 const handler = nextApp.getRequestHandler();
 
-nextApp.prepare().then(async () => {
-
+nextApp.prepare().then(() => {
   const app = express();
 
   //parse cookies
   app.use(cookieParser());
 
   // database connection
-  require('./config/db')();
+  require("./config/db")();
 
   // passport auth strategies
   require("./config/passport.config");
@@ -42,24 +39,32 @@ nextApp.prepare().then(async () => {
   app.use(require("./routes/authRoutes"));
 
   // API routes
-  app.use('/api/v1/users', require('./routes/api/v1/userRoutes'));
-  app.use('/api/v1/abuses', require('./routes/api/v1/abuseRoutes'));
-  app.use('/api/v1/accounts', require('./routes/api/v1/accountRoutes'));
-  app.use('/api/v1/auctions', require('./routes/api/v1/auctionRoutes'));
-  app.use('/api/v1/listings', require('./routes/api/v1/listingRoutes'));
-  app.use('/api/v1/bids', require('./routes/api/v1/bidRoutes'));
-  app.use('/api/v1/messages', require('./routes/api/v1/messageRoutes'));
-  app.use('/api/v1/notifications', require('./routes/api/v1/notificationRoutes'));
-  app.use('/api/v1/profiles', require('./routes/api/v1/profileRoutes'));
-  app.use('/api/v1/spaces', require('./routes/api/v1/spaceRoutes'));
-  app.use('/api/v1/user-notifications', require('./routes/api/v1/userNotificationRoutes'));
+  app.use("/api/v1/users", require("./routes/api/v1/userRoutes"));
+  app.use("/api/v1/abuses", require("./routes/api/v1/abuseRoutes"));
+  app.use("/api/v1/accounts", require("./routes/api/v1/accountRoutes"));
+  app.use("/api/v1/auctions", require("./routes/api/v1/auctionRoutes"));
+  app.use("/api/v1/listings", require("./routes/api/v1/listingRoutes"));
+  app.use("/api/v1/bids", require("./routes/api/v1/bidRoutes"));
+  app.use("/api/v1/messages", require("./routes/api/v1/messageRoutes"));
+  app.use(
+    "/api/v1/notifications",
+    require("./routes/api/v1/notificationRoutes"),
+  );
+  app.use("/api/v1/profiles", require("./routes/api/v1/profileRoutes"));
+  app.use("/api/v1/spaces", require("./routes/api/v1/spaceRoutes"));
+  app.use(
+    "/api/v1/user-notifications",
+    require("./routes/api/v1/userNotificationRoutes"),
+  );
 
   //nextjs
   app.all("*", (req, res) => handler(req, res));
 
   // use custom error handler 'errorHandler' middleware
-  app.use(require('./middlewares/errorHandler'));
+  app.use(require("./middlewares/errorHandler"));
 
-  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`.bold.cyan.underline));
-
-}).catch((err) => console.err);
+  app.listen(
+    PORT,
+    () => console.log(`Server is running on port ${PORT}`.bold.cyan.underline),
+  );
+}).catch(console.err);
